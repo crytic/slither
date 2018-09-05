ecca1233433bdf5fb4c3b75a19e0f35de3bca6cb
Josselin <josselin@trailofbits.com>
2018-09-05 23:26:47 +0100

    Update travis to use solc 0.4.24

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 
 function install_solc {
-    sudo wget -O /usr/bin/solc https://github.com/ethereum/solidity/releases/download/v0.4.23/solc-static-linux
+    sudo wget -O /usr/bin/solc https://github.com/ethereum/solidity/releases/download/v0.4.24/solc-static-linux
     sudo chmod +x /usr/bin/solc
 }
 
