5490cca41b0ae3944dd0f69161555f9721648e61
Josselin <josselin@trailofbits.com>
2018-09-05 23:22:22 +0100

    Update travis script

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-./slither.py examples/bugs --disable-solc-warnings
-if [ $? -ne 2 ]; then
+./slither.py examples/bugs/uninitialized.sol --disable-solc-warnings
+if [ $? -ne 1 ]; then
+    exit 1
+fi
+
+./slither.py examples/bugs/backdoor.sol --disable-solc-warnings
+if [ $? -ne 1 ]; then
     exit 1
 fi
 
