26ab17459cf652e118e985684a4d31e60d3c0e46
Josselin <josselin@trailofbits.com>
2018-09-13 19:10:47 +0100

    Improve uninitialized storage detector

--- examples/bugs/uninitialized_storage_pointer.sol
+++ examples/bugs/uninitialized_storage_pointer.sol
@@ -5,8 +5,10 @@ contract Uninitialized{
     }
 
     function func() payable{
-        St st;
+        St st; // non init, but never read so its fine
         St memory st2;
+        St st_bug;
+        st_bug.a;
     }    
 
 }
