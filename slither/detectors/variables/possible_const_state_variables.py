9db29140316be2c978b7758dd896a327ae4b23f6
Cryptomental <cryptomental.com@gmail.com>
2018-10-19 13:09:34 +0200

    possible_const_state_variables.py: Improve detector with more strict rules.
    Improve the detector as advised during pull request review:
    
    * contracts: iterate over contracts_derived
    * functions: iterate over contract.all_functions_called + contract.modifiers
    * check if ir.lvalue is StateVariable
    * check the initial value with slither.core.expressions.literal.Literal
    
    Refs: https://github.com/trailofbits/slither/issues/25

--- slither/detectors/variables/possible_const_state_variables.py
+++ slither/detectors/variables/possible_const_state_variables.py
@@ -2,9 +2,12 @@
 Module detecting state variables that could be declared as constant
 """
 
+from collections import defaultdict
 from slither.core.solidity_types.elementary_type import ElementaryType
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
 from slither.slithir.operations import OperationWithLValue
+from slither.core.variables.state_variable import StateVariable
+from slither.core.expressions.literal import Literal
 
 
 class ConstCandidateStateVars(AbstractDetector):
@@ -23,18 +26,17 @@ class ConstCandidateStateVars(AbstractDetector):
     @staticmethod
     def lvalues_of_operations_with_lvalue(contract):
         ret = []
-        for f in contract.functions:
-            if f.name == 'fallback':  # Skip fallback function to avoid false positives
-                continue
+        for f in contract.all_functions_called + contract.modifiers:
             for n in f.nodes:
                 for ir in n.irs:
-                    if isinstance(ir, OperationWithLValue):
+                    if isinstance(ir, OperationWithLValue) and isinstance(ir.lvalue, StateVariable):
                         ret.append(ir.lvalue)
         return ret
 
     @staticmethod
     def non_const_state_variables(contract):
-        return [variable for variable in contract.state_variables if not variable.is_constant]
+        return [variable for variable in contract.state_variables
+                if not variable.is_constant and type(variable.expression) == Literal]
 
     def detect_const_candidates(self, contract):
         const_candidates = []
@@ -52,20 +54,26 @@ class ConstCandidateStateVars(AbstractDetector):
         """ Detect state variables that could be const
         """
         results = []
-        for c in self.contracts:
+        for c in self.slither.contracts_derived:
             const_candidates = self.detect_const_candidates(c)
             if const_candidates:
-                variable_names = [v.name for v in const_candidates]
-                info = "State variables that could be const in %s, Contract: %s, Vars %s" % (self.filename,
-                                                                                             c.name,
-                                                                                             str(variable_names))
-                self.log(info)
+                variables_by_contract = defaultdict(list)
 
-                sourceMapping = [v.source_mapping for v in const_candidates]
+                for state_var in const_candidates:
+                    variables_by_contract[state_var.contract.name].append(state_var)
 
-                results.append({'vuln': 'ConstStateVariableCandidates',
-                                'sourceMapping': sourceMapping,
-                                'filename': self.filename,
-                                'contract': c.name,
-                                'unusedVars': variable_names})
+                for contract, variables in variables_by_contract.items():
+                    variable_names = [v.name for v in variables]
+                    info = "State variables that could be const in %s, Contract: %s, Vars %s" % (self.filename,
+                                                                                                 contract,
+                                                                                                 str(variable_names))
+                    self.log(info)
+
+                    sourceMapping = [v.source_mapping for v in const_candidates]
+
+                    results.append({'vuln': 'ConstStateVariableCandidates',
+                                    'sourceMapping': sourceMapping,
+                                    'filename': self.filename,
+                                    'contract': c.name,
+                                    'unusedVars': variable_names})
         return results
