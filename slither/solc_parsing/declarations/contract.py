204b5798aeb3946e3519313114524c82c2317b52
Josselin <josselin@trailofbits.com>
2018-10-23 10:28:20 +0100

    Parsing: Add .contract to Event Naming_convention detector: remove dupplicated results

--- slither/solc_parsing/declarations/contract.py
+++ slither/solc_parsing/declarations/contract.py
@@ -173,6 +173,7 @@ class ContractSolc04(Contract):
         for event_to_parse in self._eventsNotParsed:
             event = EventSolc(event_to_parse)
             event.analyze(self)
+            event.set_contract(self)
             self._events[event.full_name] = event
 
         self._eventsNotParsed = None
