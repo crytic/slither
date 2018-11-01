3b35b5a14b3ae233935480be023618f09492f6a7
Alexander Remie <user@rmi7.me>
2018-11-01 03:46:26 +0100

    force set json.dumps separators

--- scripts/pretty_print_json.py
+++ scripts/pretty_print_json.py
@@ -8,4 +8,4 @@ pretty_json_file = sys.argv[2]
 
 with open(raw_json_file, 'r') as json_data:
     with open(pretty_json_file, 'w') as out_file:
-        out_file.write(json.dumps(json.load(json_data), sort_keys=True, indent=4))
+        out_file.write(json.dumps(json.load(json_data), sort_keys=True, indent=4, separators=(',',': ')))
