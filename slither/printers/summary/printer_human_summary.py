bef529c1f11f6ecfc83be191334edcbf65c01424
Disconnect3d <dominik.b.czarnota@gmail.com>
2018-11-07 16:47:26 +0100

    Update slither/printers/summary/printer_human_summary.py
    Co-Authored-By: montyly <josselin.feist@gmail.com>

--- slither/printers/summary/printer_human_summary.py
+++ slither/printers/summary/printer_human_summary.py
@@ -19,7 +19,7 @@ class PrinterHumanSummary(AbstractPrinter):
         functions_name = [f.name for f in contract.functions]
         state_variables = [v.name for v in contract.state_variables]
 
-        if 'paused' in functions_name or 'pause' in functions_name:
+        if 'pause' in functions_name:
             txt += "\t\t Can be paused? : {}\n".format(yellow('Yes'))
         else:
             txt += "\t\t Can be paused? : {}\n".format(green('No'))
