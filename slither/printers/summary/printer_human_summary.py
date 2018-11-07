fff9db469afe1307284c93e7a5617cec700ec9d6
Disconnect3d <dominik.b.czarnota@gmail.com>
2018-11-07 16:48:01 +0100

    Update slither/printers/summary/printer_human_summary.py
    Co-Authored-By: montyly <josselin.feist@gmail.com>

--- slither/printers/summary/printer_human_summary.py
+++ slither/printers/summary/printer_human_summary.py
@@ -16,7 +16,7 @@ class PrinterHumanSummary(AbstractPrinter):
     @staticmethod
     def get_summary_erc20(contract):
         txt = ''
-        functions_name = [f.name for f in contract.functions]
+        functions_name = [f.name.lower() for f in contract.functions]
         state_variables = [v.name.lower() for v in contract.state_variables]
 
         if 'pause' in functions_name:
