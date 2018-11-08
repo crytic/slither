4eb264688c2780b33db4a2607ca775e7e6428e4c
Josselin <josselin@trailofbits.com>
2018-11-08 09:37:48 +0100

    ControlledDegatecall: improve heuristic

--- tests/controlled_delegatecall.sol
+++ tests/controlled_delegatecall.sol
@@ -1,22 +1,25 @@
 contract C{
 
-    address addr = 0x41;
+    address addr_good = 0x41;
+    address addr_bad ;
 
     bytes4 func_id;
 
     function bad_delegate_call(bytes data){
-        addr.delegatecall(data);
+        addr_good.delegatecall(data);
+        addr_bad.delegatecall(data);
     }
 
     function set(bytes4 id){
         func_id = id;
+        addr_bad = msg.sender;
     }
 
     function bad_delegate_call2(bytes data){
-        addr.delegatecall(func_id, data);
+        addr_bad.delegatecall(func_id, data);
     }
 
     function good_delegate_call(bytes data){
-        addr.delegatecall(bytes4(0x41), data);
+        addr_good.delegatecall(bytes4(0x41), data);
     }
 }
