c89329a53dd0b208b210da014d493c4b5183ef5e
Josselin <josselin@trailofbits.com>
2018-12-11 14:43:45 -0500

    Update travis install

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -4,10 +4,10 @@ python setup.py install
 pip install deepdiff
 
 function install_solc {
-    sudo wget -O /usr/bin/solc https://github.com/ethereum/solidity/releases/download/v0.4.25/solc-static-linux
-    sudo chmod +x /usr/bin/solc-4.25
-    sudo wget -O /usr/bin/solc https://github.com/ethereum/solidity/releases/download/v0.5.1/solc-static-linux
-    sudo chmod +x /usr/bin/solc-5.1
+    sudo wget -O /usr/bin/solc-0.4.25 https://github.com/ethereum/solidity/releases/download/v0.4.25/solc-static-linux
+    sudo chmod +x /usr/bin/solc-0.4.25
+    sudo wget -O /usr/bin/solc-0.5.1 https://github.com/ethereum/solidity/releases/download/v0.5.1/solc-static-linux
+    sudo chmod +x /usr/bin/solc-0.5.1
 }
 
 install_solc
