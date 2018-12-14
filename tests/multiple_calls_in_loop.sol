8bceba8986e64f8aecadf91bc5abb384af85e334
Josselin <josselin@trailofbits.com>
2018-12-14 15:06:33 -0500

    Open source detector for multiple calls in a loop

---
+++ tests/multiple_calls_in_loop.sol
@@ -0,0 +1,15 @@
+contract CallInLoop{
+
+    address[] destinations;
+
+    constructor(address[] newDestinations) public{
+        destinations = newDestinations;
+    }
+
+    function bad() external{
+        for (uint i=0; i < destinations.length; i++){
+            destinations[i].transfer(i);
+        }
+    }
+
+}
