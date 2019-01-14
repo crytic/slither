ce1f456daa441411fb257f6021f533b70724d551
Josselin <josselin@trailofbits.com>
2019-01-14 14:09:06 +0100

    SImplify version checking

--- tests/old_solc.sol
+++ tests/old_solc.sol
@@ -1,7 +1,7 @@
 // The version pragma below should get flagged by the detector
 // Reason: The expression "^0.4.0 >0.4.2" allows old solc (<0.4.23)
-pragma solidity ^0.5.22 || 0.4.23 - 0.4.24 || ^0.4.0 >0.4.2 || ~0.0.0    ^0.5.0 <= 0.5.50 || > 0.7.0;
-
+pragma solidity ^0.4.24; 
+pragma solidity >=0.4.0 <0.6.0;
 contract Contract{
 
 }
