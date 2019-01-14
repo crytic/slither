a56359d1de82fe590a51936443cd117df61719e3
Josselin <josselin@trailofbits.com>
2019-01-14 14:12:25 +0100

    Minor

--- tests/solc_version_incorrect.sol
+++ tests/solc_version_incorrect.sol
@@ -1,6 +1,5 @@
 // The version pragma below should get flagged by the detector
-// Reason: The expression "^0.4.0 >0.4.2" allows old solc (<0.4.23)
-pragma solidity ^0.4.24; 
+pragma solidity ^0.4.23; 
 pragma solidity >=0.4.0 <0.6.0;
 contract Contract{
 
