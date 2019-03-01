f7e11f4f0c670131d2610eef309573ff5867f081
Josselin <josselin@trailofbits.com>
2019-03-01 11:37:04 +0000

    possible_paths.py: create a tool (instead of a adhoc script)

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
     entry_points={
         'console_scripts': [
             'slither = slither.__main__:main',
-            'slither-check-upgradability = utils.upgradability.__main__:main'
+            'slither-check-upgradability = utils.upgradability.__main__:main',
+            'slither-find-paths = utils.possible_paths.__main__:main'
         ]
     }
 )
