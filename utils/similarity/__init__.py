f83b603a3e237cae39dd4e8635535c7f248b6ae5
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-04-20 18:18:46 -0300

    verification of sklearn (optional) dependency

--- utils/similarity/__init__.py
+++ utils/similarity/__init__.py
@@ -10,3 +10,12 @@ if fastText is None:
     print("In order to use slither-simil, you need to install fastText 0.2.0:")
     print("$ pip3 install https://github.com/facebookresearch/fastText/archive/0.2.0.zip --user")
     sys.exit(-1)
+
+try:
+    import sklearn
+except ImportError:
+    sklearn = None
+
+if sklearn is None:
+    print("In order to use plot mode in slither-simil, you need to install sklearn:")
+    print("$ pip3 install sklearn --user")
