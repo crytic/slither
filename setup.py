598af59dde7cbfc8ff4cde6acab0d93fd270e03c
Josselin <josselin@trailofbits.com>
2019-04-24 20:55:39 +0100

    v0.6.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Slither is a Solidity static analysis framework written in Python 3.',
     url='https://github.com/crytic/slither',
     author='Trail of Bits',
-    version='0.6.2',
+    version='0.6.3',
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=['prettytable>=0.7.2', 'pysha3>=1.0.2', 'crytic-compile>=0.1.0'],
