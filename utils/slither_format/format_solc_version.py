ec14b9872e6eed06b9b0358f386a7b864f235854
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-05-04 12:34:34 +0530

    Implemented logic to format correct solc-version: 0.4.25 for any version/range in 0.4.x; 0.5.3 for version/range in 0.5.x.

--- utils/slither_format/format_solc_version.py
+++ utils/slither_format/format_solc_version.py
@@ -1,14 +1,47 @@
+import re
+
 class FormatSolcVersion:
+
+    # Indicates the recommended versions for replacement
+    REPLACEMENT_VERSIONS = ["0.4.25", "0.5.3"]
+
+    # group:
+    # 0: ^ > >= < <= (optional)
+    # 1: ' ' (optional)
+    # 2: version number
+    # 3: version number
+    # 4: version number
+    PATTERN = re.compile('(\^|>|>=|<|<=)?([ ]+)?(\d+)\.(\d+)\.(\d+)')
 
     @staticmethod
     def format(slither, patches, elements):
-        # To-do: Determine which solc version to replace with
-        # If < 0.4.24 replace with 0.4.25?
-        # If > 0.5.0 replace with the latest 0.5.x?
-        solc_version_replace = "pragma solidity ^0.4.25;"
         for element in elements:
+            solc_version_replace = FormatSolcVersion.determine_solc_version_replacement(element['expression'])
             FormatSolcVersion.create_patch(slither, patches, element['source_mapping']['filename'], solc_version_replace, element['source_mapping']['start'], element['source_mapping']['start'] + element['source_mapping']['length'])
 
+    @staticmethod
+    def determine_solc_version_replacement(used_solc_version):
+        versions = FormatSolcVersion.PATTERN.findall(used_solc_version)
+        if len(versions) == 1:
+            version = versions[0]
+            minor_version = '.'.join(version[2:])[2]
+            if minor_version == '4':
+                return "pragma solidity " + FormatSolcVersion.REPLACEMENT_VERSIONS[0] + ';'
+            elif minor_version == '5':
+                return "pragma solidity " + FormatSolcVersion.REPLACEMENT_VERSIONS[1] + ';'
+            else:
+                print("Unknown version!")
+                sys.exit(-1)
+        elif len(versions) == 2:
+            version_left = versions[0]
+            version_right = versions[1]
+            minor_version_left = '.'.join(version_left[2:])[2]
+            minor_version_right = '.'.join(version_right[2:])[2]
+            if minor_version_right == '4':
+                return "pragma solidity " + FormatSolcVersion.REPLACEMENT_VERSIONS[0] + ';'
+            elif minor_version_right in ['5','6']:
+                return "pragma solidity " + FormatSolcVersion.REPLACEMENT_VERSIONS[1] + ';'
+            
     @staticmethod
     def create_patch(slither, patches, in_file, solc_version, modify_loc_start, modify_loc_end):
         in_file_str = slither.source_code[in_file]
