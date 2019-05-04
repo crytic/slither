9c5c911083231aaa6ba1410a85b83e6fccfd7a6f
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-05-04 16:55:16 +0530

    Implemented logic to format different pragma directives: 0.4.25 for directives with any version/range in 0.4.x; 0.5.3 for directives with version/range in 0.5.x. Mixing 0.4.x and 0.5.x throws exception.

--- utils/slither_format/tests/test_pragma.py
+++ utils/slither_format/tests/test_pragma.py
@@ -2,41 +2,74 @@ import unittest
 import subprocess, os, sys
   
 class TestPragma(unittest.TestCase):
-    testDataFile = "pragma.0.4.24.sol"
-    testImportFile = "pragma.0.4.23.sol"
     testDataDir = "./slither_format/tests/test_data/"
-    testFilePath = testDataDir+testDataFile
-    testImportFilePath = testDataDir+testImportFile
+    testDataFile1 = "pragma.0.4.24.sol"
+    testImportFile1 = "pragma.0.4.23.sol"
+    testFilePath1 = testDataDir+testDataFile1
+    testImportFilePath1 = testDataDir+testImportFile1
+    testDataFile2 = "pragma.0.5.4.sol"
+    testImportFile2 = "pragma.0.5.2.sol"
+    testFilePath2 = testDataDir+testDataFile2
+    testImportFilePath2 = testDataDir+testImportFile2
     
     def setUp(self):
-        outFD = open(self.testFilePath+".out","w")
-        errFD = open(self.testFilePath+".err","w")
-        p = subprocess.Popen(['python3', '-m', 'slither_format','--verbose','--detect','pragma',self.testFilePath], stdout=outFD,stderr=errFD)
-        p.wait()
-        outFD.close()
-        errFD.close()
+        outFD1 = open(self.testFilePath1+".out","w")
+        errFD1 = open(self.testFilePath1+".err","w")
+        p1 = subprocess.Popen(['python3', '-m', 'slither_format','--verbose','--detect','pragma',self.testFilePath1], stdout=outFD1,stderr=errFD1)
+        p1.wait()
+        outFD1.close()
+        errFD1.close()
+
+        outFD2 = open(self.testFilePath2+".out","w")
+        errFD2 = open(self.testFilePath2+".err","w")
+        my_env = os.environ.copy()
+        my_env["SOLC_VERSION"] = "0.5.4"
+        p2 = subprocess.Popen(['python3', '-m', 'slither_format','--verbose','--detect','pragma',self.testFilePath2], stdout=outFD2,stderr=errFD2, env=my_env)
+        p2.wait()
+        outFD2.close()
+        errFD2.close()
         
     def tearDown(self):
-        p = subprocess.Popen(['rm','-f',self.testFilePath+'.out',self.testFilePath+'.err',self.testFilePath+'.format',self.testImportFilePath+'.format'])
-        p.wait()
-        
+        p1 = subprocess.Popen(['rm','-f',self.testFilePath1+'.out',self.testFilePath1+'.err',self.testFilePath1+'.format',self.testImportFilePath1+'.format'])
+        p1.wait()
+
+        p2 = subprocess.Popen(['rm','-f',self.testFilePath2+'.out',self.testFilePath2+'.err',self.testFilePath2+'.format',self.testImportFilePath2+'.format'])
+        p2.wait()
+
     def test_pragma(self):
-        outFD = open(self.testFilePath+".out","r")
-        outFD_lines = outFD.readlines()
-        for i in range(len(outFD_lines)):
-            outFD_lines[i] = outFD_lines[i].strip()
-        self.assertTrue(os.path.isfile(self.testFilePath+".format"),"Patched .format file is not created?!")
-        self.assertEqual(outFD_lines[0],"Number of Slither results: 2")
-        self.assertEqual(outFD_lines[1],"Number of patches: 2")
-        self.assertEqual(outFD_lines.count("Detector: pragma"), 2)
-        self.assertEqual(outFD_lines.count("Old string: pragma solidity ^0.4.23;"), 1)
-        self.assertEqual(outFD_lines.count("Old string: pragma solidity ^0.4.24;"), 1)
-        self.assertEqual(outFD_lines.count("New string: pragma solidity ^0.4.25;"), 2)
-        self.assertEqual(outFD_lines.count("Location start: 0"), 2)
-        self.assertEqual(outFD_lines.count("Location end: 24"), 2)
-        self.assertEqual(outFD_lines.count("Patch file: ./slither_format/tests/test_data/pragma.0.4.24.sol"), 1)
-        self.assertEqual(outFD_lines.count("Patch file: ./slither_format/tests/test_data/pragma.0.4.23.sol"), 1)
-        outFD.close()
+        outFD1 = open(self.testFilePath1+".out","r")
+        outFD1_lines = outFD1.readlines()
+        for i in range(len(outFD1_lines)):
+            outFD1_lines[i] = outFD1_lines[i].strip()
+        self.assertTrue(os.path.isfile(self.testFilePath1+".format"),"Patched .format file is not created?!")
+        self.assertEqual(outFD1_lines[0],"Number of Slither results: 2")
+        self.assertEqual(outFD1_lines[1],"Number of patches: 2")
+        self.assertEqual(outFD1_lines.count("Detector: pragma"), 2)
+        self.assertEqual(outFD1_lines.count("Old string: pragma solidity ^0.4.23;"), 1)
+        self.assertEqual(outFD1_lines.count("Old string: pragma solidity ^0.4.24;"), 1)
+        self.assertEqual(outFD1_lines.count("New string: pragma solidity 0.4.25;"), 2)
+        self.assertEqual(outFD1_lines.count("Location start: 0"), 2)
+        self.assertEqual(outFD1_lines.count("Location end: 24"), 2)
+        self.assertEqual(outFD1_lines.count("Patch file: ./slither_format/tests/test_data/pragma.0.4.24.sol"), 1)
+        self.assertEqual(outFD1_lines.count("Patch file: ./slither_format/tests/test_data/pragma.0.4.23.sol"), 1)
+        outFD1.close()
+
+        outFD2 = open(self.testFilePath2+".out","r")
+        outFD2_lines = outFD2.readlines()
+        for i in range(len(outFD2_lines)):
+            outFD2_lines[i] = outFD2_lines[i].strip()
+        self.assertTrue(os.path.isfile(self.testFilePath2+".format"),"Patched .format file is not created?!")
+        self.assertEqual(outFD2_lines[0],"Number of Slither results: 2")
+        self.assertEqual(outFD2_lines[1],"Number of patches: 2")
+        self.assertEqual(outFD2_lines.count("Detector: pragma"), 2)
+        self.assertEqual(outFD2_lines.count("Old string: pragma solidity ^0.5.4;"), 1)
+        self.assertEqual(outFD2_lines.count("Old string: pragma solidity ^0.5.2;"), 1)
+        self.assertEqual(outFD2_lines.count("New string: pragma solidity 0.5.3;"), 2)
+        self.assertEqual(outFD2_lines.count("Location start: 0"), 2)
+        self.assertEqual(outFD2_lines.count("Location end: 23"), 2)
+        self.assertEqual(outFD2_lines.count("Patch file: ./slither_format/tests/test_data/pragma.0.5.4.sol"), 1)
+        self.assertEqual(outFD2_lines.count("Patch file: ./slither_format/tests/test_data/pragma.0.5.2.sol"), 1)
+        outFD2.close()
 
 if __name__ == '__main__':
     unittest.main()
