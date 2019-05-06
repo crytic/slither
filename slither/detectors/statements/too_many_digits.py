ebc4421f7a4a813b6f11872f196d1730194aa56a
Josselin <josselin@trailofbits.com>
2019-05-06 16:53:04 +0100

    Improve too many digits detector. Fix travis test

--- slither/detectors/statements/too_many_digits.py
+++ slither/detectors/statements/too_many_digits.py
@@ -3,52 +3,74 @@ Module detecting numbers with too many digits.
 """
 
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+from slither.slithir.variables import Constant
 
 class TooManyDigits(AbstractDetector):
     """
     Detect numbers with too many digits
     """
 
-    ARGUMENT = 'too_many_digits'  # slither will launch the detector with slither.py --too_many_digits
-    HELP = 'Numbers with too many digits (human readability risk)'
-    IMPACT = DetectorClassification.MEDIUM
+    ARGUMENT = 'too-many-digits'
+    HELP = 'Detect large digits usage'
+    IMPACT = DetectorClassification.INFORMATIONAL
     CONFIDENCE = DetectorClassification.MEDIUM
 
-    WIKI = 'https://github.com/ethereum/web3.js/blob/0.15.0/lib/utils/utils.js'
-    WIKI_TITLE = 'too_many_digits'
-    WIKI_DESCRIPTION = 'Plugin too_many_digits'
-    WIKI_EXPLOIT_SCENARIO = 'An error or malicious intent may have caused a number with many digits to be different than the number originally intended. This kind of error is usually difficult to find visually.'
-    WIKI_RECOMMENDATION = 'The number used in the contract has too many digits. Try to use an Ether denomination instead'
+    WIKI = 'https://github.com/crytic/slither/wiki/Detector-Documentation#too-many-digits'
+    WIKI_TITLE = 'Too many digits'
+    WIKI_DESCRIPTION = 'Detect large digits that do not follow best practices'
+    WIKI_EXPLOIT_SCENARIO = '''
+```solidity
+contract MyContract{
+    uint 1_ether = 10000000000000000000; 
+```
+
+While `1_ether` looks like `1 ether`, it is `10 ether`.'''
+    WIKI_RECOMMENDATION = '''
+Use:
+- [Ether suffix](https://solidity.readthedocs.io/en/latest/units-and-global-variables.html#ether-units),
+- [Time suffix](https://solidity.readthedocs.io/en/latest/units-and-global-variables.html#time-units) or,
+- [The scientific notation](https://solidity.readthedocs.io/en/latest/types.html#rational-and-integer-literals)
+'''
+
+    @staticmethod
+    def _detect_too_many_digits(f):
+        ret = []
+        for node in f.nodes:
+            # each node contains a list of IR instruction
+            for ir in node.irs:
+                # iterate over all the variables read by the IR
+                for read in ir.read:
+                    # if the variable is a constant
+                    if isinstance(read, Constant):
+                        # read.value can return an int or a str. Convert it to str
+                        value_as_str = read.original_value
+                        line_of_code = str(node.expression)
+                        if '00000' in value_as_str:
+                            # Info to be printed
+                            ret.append(node)
+        return ret
 
     def _detect(self):
         results = []
-
-        from slither import Slither
-        from slither.slithir.variables import Constant
 
         # iterate over all contracts
         for contract in self.slither.contracts_derived:
         # iterate over all functions
             for f in contract.functions:
                 # iterate over all the nodes
-                for node in f.nodes:
-                    # each node contains a list of IR instruction
-                    for ir in node.irs:
-                        # iterate over all the variables read by the IR 
-                        for read in ir.read:
-                            # if the variable is a constant
-                            if isinstance(read, Constant):
-                                # read.value can return an int or a str. Convert it to str
-                                value_as_str = str(read.value)
-                                #name_as_str = str(read.name)
-                                line_of_code = str(node.expression)
-                                if '00000' in value_as_str:
-                                    # Info to be printed
-                                    info = 'In {}.{} ({}), too many digits used in expression:\n\t- {}\n\tPlease use the proper Ether denomination instead\n'
-                                    info = info.format(contract.name, f.name, f.source_mapping_str, line_of_code)
+                ret = self._detect_too_many_digits(f)
+                if ret:
+                    info = '{}.{} ({}) uses literals with too many digits:'.format(f.contract.name,
+                                                                                   f.name,
+                                                                                   f.source_mapping_str)
+                    for node in ret:
+                        info += '\n\t- {}'.format(node.expression)
+                    info += '\n\tUse the proper denomination (ether-unit, time-unit,'
+                    info += 'or the scientific notation\n'
 
-                                    # Add the result in result
-                                    json = self.generate_json_result(info)
-                                    self.add_function_to_json(f, json)
-                                    results.append(json)
+                    # Add the result in result
+                    json = self.generate_json_result(info)
+                    self.add_nodes_to_json(ret, json)
+                    results.append(json)
+
         return results
