3876b2b479afa391f23262591a152f20498479f5
Josselin <josselin@trailofbits.com>
2019-05-07 13:45:19 +0100

    Improvements of the human summary printer. Several of the new features are integrated within slither.core or slither.utils
    - Add state_variable.signature function(s) to translate a state variable to a function signature
    - Add several contract.is_erc_* methods
    - Add contract.is_from_dependency that checks if the contract is from a dependency
    - Add utils.erc with standard ERCs signatuers
    - Add utils.standard_libraries with standard libraries
    
    Several of these methods require crytic_compile 3ed5160924d08a4d3c86d7cddf9f1e17dfe51808

--- slither/detectors/erc/incorrect_erc20_interface.py
+++ slither/detectors/erc/incorrect_erc20_interface.py
@@ -62,12 +62,12 @@ contract Token{
             list(str) : list of incorrect function signatures
         """
         # Verify this is an ERC20 contract.
-        if not contract.has_an_erc20_function():
+        if not contract.is_possible_erc20():
             return []
 
         # If this contract implements a function from ERC721, we can assume it is an ERC721 token. These tokens
         # offer functions which are similar to ERC20, but are not compatible.
-        if contract.has_an_erc721_function():
+        if contract.is_possible_erc721():
             return []
 
         functions = [f for f in contract.functions if IncorrectERC20InterfaceDetection.incorrect_erc20_interface(f.signature)]
