59f12b1376190231cfe6028f51604e6c0ad60c5d
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-05-09 14:35:50 +0530

    Added test to document that index variables of writes, _from in balances[_from] = 100;, are not captured by node._expression_vars_read of Slither and therefore are missed in naming-convention formatting.

--- utils/slither_format/tests/test_data/naming_convention_parameter.sol
+++ utils/slither_format/tests/test_data/naming_convention_parameter.sol
@@ -25,6 +25,23 @@ contract A {
     /* parameter declarations - bad */
     return (Count);
   }
-  
+
 }
+
+
+contract B {
+
+  mapping(address => uint256) balances;
+  
+    /* parameter declarations - bad */
+  function bar(address _to, address _from) returns (uint){
+    uint i;
+    /* parameter use - bad */
+    i = balances[_to];	
+    /* parameter use - bad */
+    balances[_from] = 100;
+    return(i);
+  }
+}
+
 
