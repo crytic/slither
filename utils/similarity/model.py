d3f981699960a90f92b197b78004ce6a6c4105c6
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-05-10 12:55:43 -0300

    more fixes

--- utils/similarity/model.py
+++ utils/similarity/model.py
@@ -7,5 +7,3 @@ except ImportError:
     print("ERROR: in order to use slither-simil, you need to install fastText 0.2.0:")
     print("$ pip3 install https://github.com/facebookresearch/fastText/archive/0.2.0.zip --user\n")
     sys.exit(-1)
-
-
