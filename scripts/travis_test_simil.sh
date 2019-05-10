089cb4a9e7bd51ddf2fd247858ed363c8c2ca9a5
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-05-10 11:57:23 -0300

    fixed test

--- scripts/travis_test_simil.sh
+++ scripts/travis_test_simil.sh
@@ -8,7 +8,7 @@ pip3.6 install https://github.com/facebookresearch/fastText/archive/0.2.0.zip
 ### Test slither-simil
 
 DIR_TESTS="tests/simil"
-slither-simil info "" --filename $DIR_TESTS/../complex_func.sol --contract Complex --fname complexExternalWrites --solc solc-0.4.25 > test_1.txt 2>&1
+slither-simil info "" --filename $DIR_TESTS/../complex_func.sol --fname Complex.complexExternalWrites --solc solc-0.4.25 > test_1.txt 2>&1
 DIFF=$(diff test_1.txt "$DIR_TESTS/test_1.txt")
 if [  "$DIFF" != "" ] 
 then
