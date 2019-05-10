06d70856bd60a539e29ae2e5a2e409a9b18d456f
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-05-10 12:00:22 -0300

    fix

--- utils/similarity/__init__.py
+++ utils/similarity/__init__.py
@@ -4,9 +4,6 @@ import sys
 try:
     import fastText
 except ImportError:
-    fastText = None
-
-if fastText is None:
     print("ERROR: in order to use slither-simil, you need to install fastText 0.2.0:")
     print("$ pip3 install https://github.com/facebookresearch/fastText/archive/0.2.0.zip --user\n")
     sys.exit(-1)
