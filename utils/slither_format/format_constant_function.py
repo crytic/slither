dfdb905e08c5dd32dd061434e32f46927f645615
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-05-14 15:42:30 +0530

    Refined format_constant_function to focus on the specific (view|pure|constant) string.

--- utils/slither_format/format_constant_function.py
+++ utils/slither_format/format_constant_function.py
@@ -20,17 +20,14 @@ class FormatConstantFunction:
     def create_patch(slither, patches, in_file, match_text, replace_text, modify_loc_start, modify_loc_end):
         in_file_str = slither.source_code[in_file]
         old_str_of_interest = in_file_str[modify_loc_start:modify_loc_end]
-        for match_text_item in match_text:
-            (new_str_of_interest, num_repl) = re.subn(match_text_item, replace_text, old_str_of_interest, 1)
-            if num_repl != 0:
-                break
-        if num_repl != 0:
+        m = re.search("(view|pure|constant)", old_str_of_interest)
+        if m:
             patches[in_file].append({
                 "detector" : "constant-function",
-                "start" : modify_loc_start,
-                "end" : modify_loc_end,
-                "old_string" : old_str_of_interest,
-                "new_string" : new_str_of_interest
+                "start" : modify_loc_start + m.span()[0],
+                "end" : modify_loc_start + m.span()[1],
+                "old_string" : m.groups(0)[0],
+                "new_string" : replace_text
             })
         else:
             print("Error: No view/pure/constant specifier exists. Regex failed to remove specifier!")
