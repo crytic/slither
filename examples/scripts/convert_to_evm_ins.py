b9e40b628dcec7b85940ddd5bd5862bc05c8b7e8
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-06-13 17:15:06 +0530

    Removes some unused code and adds checks and comments.

--- examples/scripts/convert_to_evm_ins.py
+++ examples/scripts/convert_to_evm_ins.py
@@ -13,17 +13,18 @@ slither = Slither(sys.argv[1])
 # Get the contract evm instructions
 contract = slither.get_contract_from_name('Test')
 contract_ins = get_evm_instructions(contract)
-print("Contract evm instructions: " + str(contract_ins))
+print("## Contract evm instructions: {} ##".format(contract.name))
+print(contract_ins)
 
 # Get the function evm instructions
 function = contract.get_function_from_signature('foo()')
+print("## Function evm instructions: {} ##".format(function.name))
 function_ins = get_evm_instructions(function)
-print("Function evm instructions: " + str(function_ins))
-
+print(function_ins)
+    
 # Get the node evm instructions
 nodes = function.nodes
 for node in nodes:
-    print("Node: " + str(node))
     node_ins = get_evm_instructions(node)
-    print("Node evm instructions: " + str(node_ins))
-
+    print("Node evm instructions: {}".format(str(node)))
+    print(node_ins)
