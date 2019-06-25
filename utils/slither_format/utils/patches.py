0dacdddaa436055027bea0ec609f743bcbb4c9f5
Josselin <josselin@trailofbits.com>
2019-06-25 10:52:36 +0200

    slither-format : use utils.patches.create_patch in naming convention

--- utils/slither_format/utils/patches.py
+++ utils/slither_format/utils/patches.py
@@ -1,10 +1,14 @@
 
 def create_patch(patches, detector, file_relative, file, start, end, old_str, new_str):
-    patches[file_relative].append({
+    p = {
         "file": file,
         "detector": detector,
         "start": start,
         "end": end,
         "old_string": old_str,
         "new_string": new_str
-    })
+    }
+    if not file_relative in patches:
+        patches[file_relative] = []
+    if p not in patches[file_relative]:
+        patches[file_relative].append(p)
