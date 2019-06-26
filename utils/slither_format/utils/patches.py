499bc67e3b431a3a282b93780d1b9382d51aa37d
Josselin <josselin@trailofbits.com>
2019-06-26 11:52:01 +0200

    Use difflib to generate patches (WIP)

--- utils/slither_format/utils/patches.py
+++ utils/slither_format/utils/patches.py
@@ -1,3 +1,4 @@
+import difflib
 
 def create_patch(patches, detector, file_relative, file, start, end, old_str, new_str):
     p = {
@@ -8,7 +9,22 @@ def create_patch(patches, detector, file_relative, file, start, end, old_str, ne
         "old_string": old_str,
         "new_string": new_str
     }
-    if not file_relative in patches:
-        patches[file_relative] = []
     if p not in patches[file_relative]:
         patches[file_relative].append(p)
+
+
+def apply_patch(original_txt, patch):
+    patched_txt = original_txt[:int(patch['start'])]
+    patched_txt += patch['new_string']
+    patched_txt += original_txt[int(patch['end']):]
+    print(patched_txt)
+    return patched_txt
+
+def create_diff(original_txt, patched_txt, filename):
+    diff = difflib.unified_diff(original_txt.splitlines(False),
+                                patched_txt.splitlines(False),
+                                fromfile=filename,
+                                tofile=filename,
+                                lineterm='')
+
+    return '\n'.join(list(diff)) + '\n'
