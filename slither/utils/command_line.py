bb57e940c30e757c18c6eb2e775f63ed6d175f38
Josselin <josselin@trailofbits.com>
2019-06-28 09:43:32 +0200

    Factorise config parsing + add config parsing to slither-format

--- slither/utils/command_line.py
+++ slither/utils/command_line.py
@@ -1,8 +1,52 @@
 import json
+import os
+import logging
 from collections import defaultdict
 from prettytable import PrettyTable
-
+from .colors import yellow, red
 from slither.detectors.abstract_detector import classification_txt
+
+logger = logging.getLogger("Slither")
+
+# Those are the flags shared by the command line and the config file
+defaults_flag_in_config = {
+    'detectors_to_run': 'all',
+    'printers_to_run': None,
+    'detectors_to_exclude': None,
+    'exclude_informational': False,
+    'exclude_low': False,
+    'exclude_medium': False,
+    'exclude_high': False,
+    'solc': 'solc',
+    'solc_args': None,
+    'disable_solc_warnings': False,
+    'json': None,
+    'truffle_version': None,
+    'disable_color': False,
+    'filter_paths': None,
+    'truffle_ignore_compile': False,
+    'truffle_build_directory': 'build/contracts',
+    'embark_ignore_compile': False,
+    'embark_overwrite_config': False,
+    # debug command
+    'legacy_ast': False,
+    'ignore_return_value': False
+    }
+
+def read_config_file(args):
+    if os.path.isfile(args.config_file):
+        try:
+            with open(args.config_file) as f:
+                config = json.load(f)
+                for key, elem in config.items():
+                    if key not in defaults_flag_in_config:
+                        logger.info(yellow('{} has an unknown key: {} : {}'.format(args.config_file, key, elem)))
+                        continue
+                    if getattr(args, key) == defaults_flag_in_config[key]:
+                        setattr(args, key, elem)
+        except json.decoder.JSONDecodeError as e:
+            logger.error(red('Impossible to read {}, please check the file {}'.format(args.config_file, e)))
+
 
 def output_to_markdown(detector_classes, printer_classes, filter_wiki):
 
