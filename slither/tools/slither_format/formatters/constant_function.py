7d42faad1e06fa3b5e6c762a9a6c6f2488a071dd
Josselin <josselin@trailofbits.com>
2019-09-11 11:11:01 +0200

    Fix constant function patch

--- slither/tools/slither_format/formatters/constant_function.py
+++ slither/tools/slither_format/formatters/constant_function.py
@@ -2,7 +2,8 @@ import re
 from ..exceptions import FormatError
 from ..utils.patches import create_patch
 
-def format(slither, patches, elements):
+def format(slither, result):
+    elements = result['elements']
     for element in elements:
         if element['type'] != "function":
             # Skip variable elements
@@ -11,21 +12,21 @@ def format(slither, patches, elements):
         if target_contract:
             for function in target_contract.functions:
                 if function.name == element['name']:
-                    _patch(slither, patches,
+                    _patch(slither, result,
                            element['source_mapping']['filename_absolute'],
-                           element['source_mapping']['filename_relative'],
-                           int(function.parameters_src.source_mapping['start']),
+                           int(function.parameters_src.source_mapping['start'] +
+                               function.parameters_src.source_mapping['length']),
                            int(function.returns_src.source_mapping['start']))
                     break
 
 
-def _patch(slither, patches, in_file, in_file_relative, modify_loc_start, modify_loc_end):
+def _patch(slither, result, in_file, modify_loc_start, modify_loc_end):
     in_file_str = slither.source_code[in_file].encode('utf8')
     old_str_of_interest = in_file_str[modify_loc_start:modify_loc_end]
     # Find the keywords view|pure|constant and remove them
     m = re.search("(view|pure|constant)", old_str_of_interest.decode('utf-8'))
     if m:
-        create_patch(patches,
+        create_patch(result,
                      in_file,
                      modify_loc_start + m.span()[0],
                      modify_loc_start + m.span()[1],
