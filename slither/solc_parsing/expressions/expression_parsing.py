d274ed6c63a6b9cd027528f1700ab186981d7d1d
Josselin <josselin@trailofbits.com>
2019-09-11 09:52:39 +0200

    Add IR conversion for constant state variable conversion

--- slither/solc_parsing/expressions/expression_parsing.py
+++ slither/solc_parsing/expressions/expression_parsing.py
@@ -569,7 +569,9 @@ def parse_expression(expression, caller_context):
             var = find_variable(super_name, caller_context, is_super=True)
             if var is None:
                 raise VariableNotFound('Variable not found: {}'.format(super_name))
-            return SuperIdentifier(var)
+            sup = SuperIdentifier(var)
+            sup.set_offset(expression['src'], caller_context.slither)
+            return sup
         member_access = MemberAccess(member_name, member_type, member_expression)
         if str(member_access) in SOLIDITY_VARIABLES_COMPOSED:
             return Identifier(SolidityVariableComposed(str(member_access)))
