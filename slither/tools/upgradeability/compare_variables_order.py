6a04454100f45036ef2a9a3ac67e69fb9ce309c0
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-10-16 14:23:47 +0530

    Starts adding json formatting of results.

--- slither/tools/upgradeability/compare_variables_order.py
+++ slither/tools/upgradeability/compare_variables_order.py
@@ -6,11 +6,15 @@ from slither import Slither
 from slither.utils.function import get_function_id
 from slither.utils.colors import red, green, yellow
 
+from collections import OrderedDict
+
 logger = logging.getLogger("VariablesOrder")
 logger.setLevel(logging.INFO)
 
 def compare_variables_order_implementation(v1, contract_name1, v2, contract_name2):
 
+    results = OrderedDict()
+    
     logger.info(green('Run variables order checks between implementations... (see https://github.com/crytic/slither/wiki/Upgradeability-Checks#variables-order-checks)'))
 
     contract_v1 = v1.get_contract_from_name(contract_name1)
@@ -32,16 +36,17 @@ def compare_variables_order_implementation(v1, contract_name1, v2, contract_name
     for idx in range(0, len(order_v1)):
         (v1_name, v1_type) =  order_v1[idx]
         if len(order_v2) < idx:
-            logger.info(red('Missing variable in the new version: {} {}'.format(v1_name, v1_type)))
+            info = 'Missing variable in the new version: {} {}'.format(v1_name, v1_type)
+            logger.info(red(info))
+            results['missing_variable'] = info
             continue
         (v2_name, v2_type) =  order_v2[idx]
 
         if (v1_name != v2_name) or (v1_type != v2_type):
             found = True
-            logger.info(red('Different variables between v1 and v2: {} {} -> {} {}'.format(v1_name,
-                                                                        v1_type,
-                                                                        v2_name,
-                                                                        v2_type)))
+            info = 'Different variables between v1 and v2: {} {} -> {} {}'.format(v1_name, v1_type, v2_name, v2_type)
+            logger.info(red(info))
+            results['different_variables'] = info
 
     if len(order_v2) > len(order_v1):
         new_variables = order_v2[len(order_v1):]
@@ -74,16 +79,20 @@ def compare_variables_order_proxy(implem, implem_name, proxy, proxy_name):
     for idx in range(0, len(order_proxy)):
         (proxy_name, proxy_type) =  order_proxy[idx]
         if len(order_implem) <= idx:
-            logger.info(red('Extra variable in the proxy: {} {}'.format(proxy_name, proxy_type)))
+            info = 'Extra variable in the proxy: {} {}'.format(proxy_name, proxy_type)
+            logger.info(red(info))
+            results['extra_variable'] = info
             continue
         (implem_name, implem_type) =  order_implem[idx]
 
         if (proxy_name != implem_name) or (proxy_type != implem_type):
             found = True
-            logger.info(red('Different variables between proxy and implem: {} {} -> {} {}'.format(proxy_name,
-                                                                        proxy_type,
-                                                                        implem_name,
-                                                                        implem_type)))
+            info = 'Different variables between proxy and implem: {} {} -> {} {}'.format(proxy_name,
+                                                                                         proxy_type,
+                                                                                         implem_name,
+                                                                                         implem_type)
+            logger.info(red(info))
+            results['different_variable'] = info
         else:
             logger.info(yellow('Variable in the proxy: {} {}'.format(proxy_name,
                                                                      proxy_type)))
