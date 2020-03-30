93d4611becc73e9db9d6c24de1e7fdbca8d7dec1
Josselin <josselin@trailofbits.com>
2020-03-30 15:36:43 +0200

    Improve expression text output if {gas:X, value:Y} is used

--- slither/core/expressions/call_expression.py
+++ slither/core/expressions/call_expression.py
@@ -44,4 +44,14 @@ class CallExpression(Expression):
         return self._type_call
 
     def __str__(self):
-        return str(self._called) + '(' + ','.join([str(a) for a in self._arguments]) + ')'
+        txt = str(self._called)
+        if self.call_gas or self.call_value:
+            gas = f'gas: {self.call_gas}' if self.call_gas else ''
+            value = f'value: {self.call_value}' if self.call_value else ''
+            if gas and value:
+                txt += '{' + f'{gas}, {value}' + '}'
+            elif gas:
+                txt += '{' + f'{gas}' + '}'
+            else:
+                txt += '{' + f'{value}' + '}'
+        return txt + '(' + ','.join([str(a) for a in self._arguments]) + ')'
