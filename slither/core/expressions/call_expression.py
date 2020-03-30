bd08e083c9fbd5a3930c7f6c16ac65e6b3325a03
Josselin <josselin@trailofbits.com>
2020-03-30 15:30:57 +0200

    Add suport for new call syntax: {gas: X, value: Y} (fix #419)

--- slither/core/expressions/call_expression.py
+++ slither/core/expressions/call_expression.py
@@ -1,5 +1,6 @@
 from slither.core.expressions.expression import Expression
 
+
 class CallExpression(Expression):
 
     def __init__(self, called, arguments, type_call):
@@ -8,6 +9,27 @@ class CallExpression(Expression):
         self._called = called
         self._arguments = arguments
         self._type_call = type_call
+        # gas and value are only available if the syntax is {gas: , value: }
+        # For the .gas().value(), the member are considered as function call
+        # And converted later to the correct info (convert.py)
+        self._gas = None
+        self._value = None
+
+    @property
+    def call_value(self):
+        return self._value
+
+    @call_value.setter
+    def call_value(self, v):
+        self._value = v
+
+    @property
+    def call_gas(self):
+        return self._gas
+
+    @call_gas.setter
+    def call_gas(self, gas):
+        self._gas = gas
 
     @property
     def called(self):
@@ -23,4 +45,3 @@ class CallExpression(Expression):
 
     def __str__(self):
         return str(self._called) + '(' + ','.join([str(a) for a in self._arguments]) + ')'
-
