6c7ce89db22b604e5e3f5ebfd859931586f89c83
Josselin <josselin@trailofbits.com>
2020-03-31 14:58:39 +0200

    Refactor reentrancy to show where are the calls/write to variables in internal calls (fix #412)

--- slither/detectors/reentrancy/reentrancy_eth.py
+++ slither/detectors/reentrancy/reentrancy_eth.py
@@ -4,10 +4,16 @@
     Based on heuristics, it may lead to FP and FN
     Iterate over all the nodes of the graph until reaching a fixpoint
 """
+from collections import namedtuple, defaultdict
+from typing import List
+
 from slither.detectors.abstract_detector import DetectorClassification
+from .reentrancy import Reentrancy, to_hashable
+
+FindingKey = namedtuple('FindingKey', ['function', 'calls', 'send_eth'])
+FindingValue = namedtuple('FindingValue', ['variable', 'node', 'nodes'])
 
 
-from .reentrancy import Reentrancy
 class ReentrancyEth(Reentrancy):
     ARGUMENT = 'reentrancy-eth'
     HELP = 'Reentrancy vulnerabilities (theft of ethers)'
@@ -34,13 +40,12 @@ Do not report reentrancies that don't involve ethers (see `reentrancy-no-eth`)''
 
 Bob uses the re-entrancy bug to call `withdrawBalance` two times, and withdraw more than its initial deposit to the contract.'''
 
-
     WIKI_RECOMMENDATION = 'Apply the [check-effects-interactions pattern](http://solidity.readthedocs.io/en/v0.4.21/security-considerations.html#re-entrancy).'
 
     STANDARD_JSON = False
 
     def find_reentrancies(self):
-        result = {}
+        result = defaultdict(set)
         for contract in self.contracts:
             for f in contract.functions_and_modifiers_declared:
                 for node in f.nodes:
@@ -48,24 +53,26 @@ Bob uses the re-entrancy bug to call `withdrawBalance` two times, and withdraw m
                     if not self.KEY in node.context:
                         continue
                     if node.context[self.KEY].calls and node.context[self.KEY].send_eth:
-                        if not any(n!=node for n in node.context[self.KEY].send_eth):
+                        if not any(n != node for n in node.context[self.KEY].send_eth):
                             continue
-                        read_then_written = []
+                        read_then_written = set()
                         for c in node.context[self.KEY].calls:
                             if c == node:
                                 continue
-                            read_then_written += [(v, node) for v in node.context[self.KEY].written
-                                                 if v in node.context[self.KEY].reads_prior_calls[c]]
+                            read_then_written |= set([FindingValue(v,
+                                                                   node,
+                                                                   tuple(sorted(nodes, key=lambda x: x.node_id)))
+                                                      for (v, nodes)
+                                                      in node.context[self.KEY].written.items()
+                                                      if v in node.context[self.KEY].reads_prior_calls[c]])
 
                         if read_then_written:
                             # calls are ordered
-                            finding_key = (node.function,
-                                           tuple(sorted(list(node.context[self.KEY].calls), key=lambda x:x.node_id)),
-                                           tuple(sorted(list(node.context[self.KEY].send_eth), key=lambda x:x.node_id)))
-                            finding_vars = read_then_written
-                            if finding_key not in result:
-                                result[finding_key] = []
-                            result[finding_key] = list(set(result[finding_key] + finding_vars))
+                            finding_key = FindingKey(function=node.function,
+                                                     calls=to_hashable(node.context[self.KEY].calls),
+                                                     send_eth=to_hashable(node.context[self.KEY].send_eth))
+
+                            result[finding_key] |= set(read_then_written)
         return result
 
     def _detect(self):
@@ -77,22 +84,33 @@ Bob uses the re-entrancy bug to call `withdrawBalance` two times, and withdraw m
 
         results = []
 
-        result_sorted = sorted(list(reentrancies.items()), key=lambda x:x[0][0].name)
+        result_sorted = sorted(list(reentrancies.items()), key=lambda x: x[0].function.name)
+        varsWritten: List[FindingValue]
         for (func, calls, send_eth), varsWritten in result_sorted:
-            calls = sorted(list(set(calls)), key=lambda x: x.node_id)
-            send_eth = sorted(list(set(send_eth)), key=lambda x: x.node_id)
+            calls = sorted(list(set(calls)), key=lambda x: x[0].node_id)
+            send_eth = sorted(list(set(send_eth)), key=lambda x: x[0].node_id)
+            varsWritten = sorted(varsWritten, key=lambda x: (x.variable.name, x.node.node_id))
 
             info = ['Reentrancy in ', func, ':\n']
             info += ['\tExternal calls:\n']
-            for call_info in calls:
-                info += ['\t- ' , call_info, '\n']
+            for (call_info, calls_list) in calls:
+                info += ['\t- ', call_info, '\n']
+                for call_list_info in calls_list:
+                    if call_list_info != call_info:
+                        info += ['\t\t- ', call_list_info, '\n']
             if calls != send_eth and send_eth:
                 info += ['\tExternal calls sending eth:\n']
-                for call_info in send_eth:
+                for (call_info, calls_list) in send_eth:
                     info += ['\t- ', call_info, '\n']
+                    for call_list_info in calls_list:
+                        if call_list_info != call_info:
+                            info += ['\t\t- ', call_list_info, '\n']
             info += ['\tState variables written after the call(s):\n']
-            for (v, node) in sorted(varsWritten, key=lambda x: (x[0].name, x[1].node_id)):
-                info += ['\t- ', v, ' in ', node, '\n']
+            for finding_value in varsWritten:
+                info += ['\t- ', finding_value.node, '\n']
+                for other_node in finding_value.nodes:
+                    if other_node != finding_value.node:
+                        info += ['\t\t- ', other_node, '\n']
 
             # Create our JSON result
             res = self.generate_result(info)
@@ -101,26 +119,40 @@ Bob uses the re-entrancy bug to call `withdrawBalance` two times, and withdraw m
             res.add(func)
 
             # Add all underlying calls in the function which are potentially problematic.
-            for call_info in calls:
+            for (call_info, calls_list) in calls:
                 res.add(call_info, {
                     "underlying_type": "external_calls"
                 })
-
-            #
+                for call_list_info in calls_list:
+                    if call_list_info != call_info:
+                        res.add(call_list_info, {
+                            "underlying_type": "external_calls_sending_eth"
+                        })
 
             # If the calls are not the same ones that send eth, add the eth sending nodes.
             if calls != send_eth:
-                for call_info in send_eth:
+                for (call_info, calls_list) in send_eth:
                     res.add(call_info, {
                         "underlying_type": "external_calls_sending_eth"
                     })
+                    for call_list_info in calls_list:
+                        if call_list_info != call_info:
+                            res.add(call_list_info, {
+                                "underlying_type": "external_calls_sending_eth"
+                            })
 
             # Add all variables written via nodes which write them.
-            for (v, node) in varsWritten:
-                res.add(node, {
+            for finding_value in varsWritten:
+                res.add(finding_value.node, {
                     "underlying_type": "variables_written",
-                    "variable_name": v.name
+                    "variable_name": finding_value.variable.name
                 })
+                for other_node in finding_value.nodes:
+                    if other_node != finding_value.node:
+                        res.add(other_node, {
+                            "underlying_type": "variables_written",
+                            "variable_name": finding_value.variable.name
+                        })
 
             # Append our result
             results.append(res)
