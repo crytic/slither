981d0a17e017e4699a0e1075d5318229f118a259
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2020-07-03 04:11:28 -0400

    Latest version of FastText was successfully tested with Slither-simil.

--- slither/tools/similarity/model.py
+++ slither/tools/similarity/model.py
@@ -1,9 +1,11 @@
 import sys
 
 try:
-    from fastText import load_model
-    from fastText import train_unsupervised
+    from fasttext import load_model
+    from fasttext import train_unsupervised
 except ImportError:
-    print("ERROR: in order to use slither-simil, you need to install fastText 0.2.0:")
-    print("$ pip3 install https://github.com/facebookresearch/fastText/archive/0.2.0.zip --user\n")
+    #print("ERROR: in order to use slither-simil, you need to install fasttext 0.2.0:")
+    #print("$ pip3 install https://github.com/facebookresearch/fasttext/archive/0.2.0.zip --user\n")
+    print("ERROR: in order to use slither-simil, you need to install the latest fasttext version (Currently test with 0.9.2 [Apr 28, 2020]):")
+    print("$ git clone https://github.com/facebookresearch/fastText.git\n$cd fastText\npipi3 install . --user\n")
     sys.exit(-1)
