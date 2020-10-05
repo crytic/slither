2ee2792064fef5f7fdc9646836509d8627a373e4
Josselin <josselin@trailofbits.com>
2020-10-05 13:07:31 +0200

    Refactor detectors tests to use pytest

--- scripts/ci_test_detectors_6.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname "$0")""/ci_test.sh"
-
-solc use "0.6.11"
-
-# test_slither tests/filename.sol "detector_name"
--- scripts/ci_test_detectors_7.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname "$0")""/ci_test.sh"
-
-solc use "0.7.0"
-
-# test_slither tests/filename.sol "detector_name"
