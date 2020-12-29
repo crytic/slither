c8a1115fb96c74daea2b85d2ae02cc1bb43f2e28
Maximilian Krueger <kruemaxi@gmail.com>
2020-12-29 12:14:09 -0600

    add "payable" printer

---
+++ slither/printers/functions/payable.py
@@ -0,0 +1,33 @@
+from slither.printers.abstract_printer import AbstractPrinter
+from slither.utils.myprettytable import MyPrettyTable
+
+
+class Payable(AbstractPrinter):
+
+    ARGUMENT = "payable"
+    HELP = "Print all functions which can receive ether"
+
+    WIKI = "https://github.com/trailofbits/slither/wiki/Printer-documentation#payable"
+
+    def output(self, _filename: str):
+        txt = ""
+        all_tables = []
+        for contract in self.slither.contracts:
+            if contract.is_top_level:
+                continue
+            payable_functions = [f for f in contract.functions if f.payable]
+            if payable_functions:
+                txt += f"\n{contract.name}:\n"
+                table = MyPrettyTable(["Name"])
+                for function in payable_functions:
+                    table.add_row([function.solidity_signature])
+                txt += str(table) + "\n"
+                all_tables.append((contract.name, table))
+
+        self.info(txt)
+
+        res = self.generate_output(txt)
+        for name, table in all_tables:
+            res.add_pretty_table(table, name)
+
+        return res
