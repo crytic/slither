eb4363a172e72fb850dcf42703aad42190ef49dc
Maximilian Krueger <kruemaxi@gmail.com>
2021-01-05 21:16:41 -0600

    add more tests for Function.can_send_eth

--- tests/test_function.sol
+++ tests/test_function.sol
@@ -54,3 +54,76 @@ contract TestFunction {
         return true;
     }
 }
+
+contract TestFunctionCanSendEth {
+
+    function send_direct() internal {
+        address(1).send(1);
+    }
+
+    function transfer_direct() internal {
+        address(1).transfer(1);
+    }
+
+    function call_direct() internal {
+        address(1).call{value: 1}("");
+    }
+
+    function highlevel_call_direct() internal {
+        TestFunctionCanSendEthOther(address(5)).i_am_payable{value: 1}();
+    }
+
+    function send_via_internal() public {
+        send_direct();
+    }
+
+    function transfer_via_internal() public {
+        transfer_direct();
+    }
+
+    function call_via_internal() public {
+        call_direct();
+    }
+
+    function highlevel_call_via_internal() public {
+        highlevel_call_direct();
+    }
+
+    function send_via_external() public {
+        TestFunctionCanSendEthOther(address(5)).send_direct();
+    }
+
+    function transfer_via_external() public {
+        TestFunctionCanSendEthOther(address(5)).transfer_direct();
+    }
+
+    function call_via_external() public {
+        TestFunctionCanSendEthOther(address(5)).call_direct();
+    }
+
+    function highlevel_call_via_external() public {
+        TestFunctionCanSendEthOther(address(5)).highlevel_call_direct();
+    }
+}
+
+contract TestFunctionCanSendEthOther {
+    function i_am_payable() external payable {
+
+    }
+
+    function send_direct() external {
+        address(1).send(1);
+    }
+
+    function transfer_direct() external {
+        address(1).transfer(1);
+    }
+
+    function call_direct() external {
+        address(1).call{value: 1}("");
+    }
+
+    function highlevel_call_direct() external {
+        TestFunctionCanSendEthOther(address(5)).i_am_payable{value: 1}();
+    }
+}
