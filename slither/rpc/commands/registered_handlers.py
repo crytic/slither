7602ab7e7d2737a280e1a8dbadc1d5507f1eedb3
David Pokora <dpokora@gmail.com>
2021-06-01 09:46:52 -0400

    Small pylint tweak

--- slither/rpc/commands/registered_handlers.py
+++ slither/rpc/commands/registered_handlers.py
@@ -1,2 +1,3 @@
+# pylint: disable=unused-import,relative-beyond-top-level
 from slither.rpc.commands.base_handler import BaseCommandHandler
 from slither.rpc.commands.bootstrap.initialize_handler import InitializeHandler
