1548c8217e2a537f8ccb990c879836b571477112
Feist Josselin <josselin@trailofbits.com>
2021-11-30 20:48:13 +0100

    Improve support for yul's slot direct usage (#983)
    * Improve support for yul's slot direct usage
    * Add a lot of expected json files for the AST testing that were missing

---
+++ tests/ast-parsing/yul-0.8.0.sol
@@ -0,0 +1,52 @@
+library L {
+
+}
+
+contract C {
+    uint storA;
+    uint[] storB;
+    bool storC;
+
+    function f(uint paramA, uint[] memory paramB) public returns (uint retA, uint[] memory retB) {
+        uint localA;
+        uint[] memory localB;
+
+        assembly {
+            let aStorA := sload(storA.slot)
+            let aParamA := paramA
+            let aRetA := retA
+            let aLocalA := localA
+
+            sstore(storA.slot, 0)
+            paramA := 0
+            retA := 0
+            localA := 0
+
+            let aStorB := sload(storB.slot)
+            let aParamB := mload(paramB)
+            let aRetB := mload(retB)
+            let aLocalB := mload(localB)
+
+            sstore(storB.slot, 0)
+            mstore(paramB, 0)
+            mstore(retB, 0)
+            mstore(localB, 0)
+
+            let aStoreC := mul(sload(storC.slot), storC.offset)
+
+            // let libAddr := L // Not supported for now
+        }
+    }
+
+
+    struct St{
+        uint a;
+    }
+
+    function f() internal returns(St storage st){
+        assembly{
+            st.slot := 0x0000000000000000000000000000000000000000000000000000000000000000
+        }
+    }
+
+}
