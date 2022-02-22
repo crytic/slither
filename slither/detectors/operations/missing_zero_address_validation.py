d5caa6f4207558ba721bd2fa673b5a13acac4ab7
Jaime <22358726+Jaime-Iglesias@users.noreply.github.com>
2022-02-22 15:00:07 +0000

    working on library support

--- slither/detectors/operations/missing_zero_address_validation.py
+++ slither/detectors/operations/missing_zero_address_validation.py
@@ -2,14 +2,77 @@
 Module detecting missing zero address validation
 
 """
-from collections import defaultdict
+from typing import List, Set
+from slither.core.declarations.contract import Contract
+from slither.core.declarations.function_contract import FunctionContract
+from slither.core.variables.variable import Variable
 
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
-from slither.analyses.data_dependency.data_dependency import is_tainted, is_dependent
+from slither.analyses.data_dependency.data_dependency import is_dependent
 from slither.core.solidity_types.elementary_type import ElementaryType
-from slither.slithir.operations import Send, Transfer, LowLevelCall
-from slither.slithir.operations import Call
+from slither.slithir.operations.binary import Binary, BinaryType
+from slither.slithir.operations.internal_call import InternalCall
+from slither.slithir.operations.library_call import LibraryCall
+from slither.slithir.operations.type_conversion import TypeConversion
+from slither.slithir.variables.constant import Constant
+from slither.utils.output import Output
 
+def _collect(contract: Contract, targets: List[Variable], zero_addresses: List[Variable]) -> None:
+    """
+        TODO: Add description
+    """
+
+    for function in contract.functions:
+        for ir in function.slithir_operations:
+            if isinstance(ir, TypeConversion) and ir.type == ElementaryType("address") and ir.variable == Constant("0"):
+                if ir.lvalue not in zero_addresses:
+                    zero_addresses.append(ir.lvalue)
+
+        for param in function.parameters:
+            if param.type == ElementaryType("address") and param not in targets:
+                targets.append(param)
+
+        # We also need to check the modifiers
+        for mod in function.modifiers:
+            for param in mod.parameters:
+                if param.type == ElementaryType("address") and param not in targets:
+                    targets.append(param)
+
+            for ir in mod.all_slithir_operations():
+                if isinstance(ir, TypeConversion) and ir.type == ElementaryType("address") and ir.variable == Constant("0"):
+                    if ir.lvalue not in zero_addresses:
+                        zero_addresses.append(ir.lvalue)
+
+def _performs_address_check(function: FunctionContract, targets: List[Variable], zero_addresses: List[Variable], removed: List[Variable]) -> bool:
+    """
+        TODO: Add description
+    """
+
+    performs_check: bool = False
+
+    for ir in function.all_slithir_operations():
+        # If it is a binary operation
+        if isinstance(ir, Binary):
+            if ir.type == BinaryType.EQUAL or ir.type == BinaryType.NOT_EQUAL:
+                if ir.variable_left in targets and (ir.variable_right in zero_addresses or ir.variable_right == 0):
+                    for t in targets:
+                        if t != ir.variable_left and is_dependent(ir.variable_left, t, ir.function) and t not in removed:
+                            removed.append(t)
+                    if ir.variable_left not in removed:
+                        removed.append(ir.variable_left)
+                    performs_check = True
+
+        # If an internal call or a library call was made
+        elif isinstance(ir, InternalCall) or isinstance(ir, LibraryCall):
+            if _performs_address_check(ir.function, targets, zero_addresses, removed):
+                for t in targets:
+                    if t not in removed and t in ir.arguments:
+                        for r in removed:
+                            if r != t and is_dependent(r, t, ir.function.contract):
+                                if t not in removed:
+                                    removed.append(t)
+                performs_check = True
+    return performs_check
 
 class MissingZeroAddressValidation(AbstractDetector):
     """
@@ -40,133 +103,43 @@ contract C {
   }
 }
 ```
-Bob calls `updateOwner` without specifying the `newOwner`, soBob loses ownership of the contract.
+Bob calls `updateOwner` without specifying the `newOwner`, so Bob loses ownership of the contract.
 """
     # endregion wiki_exploit_scenario
 
     WIKI_RECOMMENDATION = "Check that the address is not zero."
 
-    def _zero_address_validation_in_modifier(self, var, modifier_exprs):
-        for mod in modifier_exprs:
-            for node in mod.nodes:
-                # Skip validation if the modifier's parameters contains more than one variable
-                # For example
-                # function f(a) my_modif(some_internal_function(a, b)) {
-                if len(node.irs) != 1:
-                    continue
-                args = [arg for ir in node.irs if isinstance(ir, Call) for arg in ir.arguments]
-                # Check in modifier call arguments and then identify validation of corresponding parameter within modifier context
-                if var in args and self._zero_address_validation(
-                    mod.modifier.parameters[args.index(var)], mod.modifier.nodes[-1], []
-                ):
-                    return True
-        return False
-
-    def _zero_address_validation(self, var, node, explored):
+    def _detect_missing_zero_address_validation(self, contract: Contract, targets: List[Variable], zero_addresses: List[Variable], removed: List[Variable]) -> None:
         """
-        Detects (recursively) if var is (zero address) checked in the function node
+            TODO: Add description
         """
-        if node in explored:
-            return False
-        explored.append(node)
+        if targets:
+            for function in contract.functions:
+                # Perform check inside the function
+                _performs_address_check(function, targets, zero_addresses, removed)
 
-        # Heuristic: Assume zero address checked if variable is used within conditional or require/assert
-        # TBD: Actually check for zero address in predicate
-        if node.contains_if():
-            if var in node.variables_read:
-                return True
-            # check dependant variables
-        if node.contains_require_or_assert():
-            for v in node.variables_read:
-                if is_dependent(var, v, node.function) or is_dependent(v, var, node.function):
-                    return True
-
-        # Check recursively in all the parent nodes
-        for father in node.fathers:
-            if self._zero_address_validation(var, father, explored):
-                return True
-        return False
-
-    def _zero_address_validation_icalls(self, var, node) -> bool:
-        """Check if the interesting variable `var` is checked inside any `Call` expression
-        in this `node`
-        """
-        if node.type == node.type.EXPRESSION:
-            for ir in node.irs:
-                if isinstance(ir, Call):
-                    args = ir.arguments
-                    # the interesting variable is passed to something
-                    if var in args:
-                        if self._zero_address_validation(
-                            ir.function.parameters[args.index(var)], ir.function.nodes[-1], []
-                        ):
-                            return True
-        return False
-
-    def _detect_missing_zero_address_validation(self, contract):
-        """
-        Detects if addresses are zero address validated before use.
-        :param contract: The contract to check
-        :return: Functions with nodes where addresses used are not zero address validated earlier
-        """
-        results = []
-
-        for function in contract.functions_entry_points:
-            var_nodes = defaultdict(list)
-
-            for node in function.nodes:
-                sv_addrs_written = [
-                    sv
-                    for sv in node.state_variables_written
-                    if sv.type == ElementaryType("address")
-                ]
-
-                addr_calls = False
-                for ir in node.irs:
-                    if isinstance(ir, (Send, Transfer, LowLevelCall)):
-                        addr_calls = True
-
-                # Continue if no address-typed state variables are written and if no send/transfer/call
-                if not sv_addrs_written and not addr_calls:
-                    continue
-
-                # Check local variables used in such nodes
-                for var in node.local_variables_read:
-                    # Check for address types that are tainted but not by msg.sender
-                    if var.type == ElementaryType("address") and is_tainted(
-                        var, function, ignore_generic_taint=True
-                    ):
-                        if self._zero_address_validation_icalls(var, node):
-                            continue
-                        # Check for zero address validation of variable
-                        # in the context of modifiers used or prior function context
-                        if not (
-                            self._zero_address_validation_in_modifier(
-                                var, function.modifiers_statements
-                            )
-                            or self._zero_address_validation(var, node, [])
-                        ):
-                            # Report a variable only once per function
-                            var_nodes[var].append(node)
-            if var_nodes:
-                results.append((function, var_nodes))
-        return results
-
-    def _detect(self):
+    def _detect(self) -> List[Output]:
         """Detect if addresses are zero address validated before use.
         Returns:
             list: {'(function, node)'}
         """
 
         # Check derived contracts for missing zero address validation
-        results = []
+        results: List[Output] = []
+        targets: List[Variable] = []
+        zero_addresses: List[Variable] = []
+        removed: List[Variable] = []
+
         for contract in self.compilation_unit.contracts_derived:
-            missing_zero_address_validation = self._detect_missing_zero_address_validation(contract)
-            for (_, var_nodes) in missing_zero_address_validation:
-                for var, nodes in var_nodes.items():
-                    info = [var, " lacks a zero-check on ", ":\n"]
-                    for node in nodes:
-                        info += ["\t\t- ", node, "\n"]
-                    res = self.generate_result(info)
-                    results.append(res)
+            _collect(contract, targets, zero_addresses)
+
+            self._detect_missing_zero_address_validation(contract, targets, zero_addresses, removed)
+
+        missing_zero_address_validation = list(set(targets) - set(removed))
+
+        for var in missing_zero_address_validation:
+            info = [var, " lacks a zero check "]
+            res = self.generate_result(info)
+            results.append(res)
+
         return results
