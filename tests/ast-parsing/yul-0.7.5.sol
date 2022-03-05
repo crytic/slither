508745430bf578471b649bc7db76b52ea1886254
Josselin <josselin@trailofbits.com>
2022-03-05 17:57:01 +0100

    Fix broken tests + minor

--- tests/ast-parsing/yul-0.7.5.sol
+++ tests/ast-parsing/yul-0.7.5.sol
@@ -2,7 +2,7 @@ contract C {
     function f(bytes calldata paramA) external returns (uint256 retA) {
         assembly {
             retA := paramA.offset
-            retA := add(retA, batchData.length)
+            retA := add(retA, paramA.length)
         }
     }
 
