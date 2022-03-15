a5470586acf8b6770d0a0c73865f17059945a14b
Boyan-MILANOV <boyan.milanov@trailofbits.com>
2022-03-15 11:53:33 +0100

    Update RTLO detector test

--- tests/detectors/rtlo/0.8.0/unicode_direction_override.sol
+++ tests/detectors/rtlo/0.8.0/unicode_direction_override.sol
@@ -1,15 +1,11 @@
 pragma solidity ^0.8.0;
-contract C {
-    function f() external pure
+contract my_contract {
+    function empty_func() external pure
     {
-        // RLO PDF
-        /*ok ‮‬*/
-
-        // RLO RLO PDF PDF
-        /*ok ‮‮‬‬*/
-
-        // RLO RLO RLO PDF PDF PDF
-        /*ok ‮‮‮‬‬‬*/
+        // The string below contains 3 RLO and 3 PDF unicode characters
+        // RLO is U+202E and changes the print direction to right-to-left
+        // PDF is U+202C and restores the print direction to what it was before RLO
+        /*ok ‮aaa‮bbb‮ccc‬ddd‬eee‬*/
     }
 }
 // ----
