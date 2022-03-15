963ecc9bd9b6b6b5388358dddfe3e90f9e8bd195
Boyan-MILANOV <boyan.milanov@trailofbits.com>
2022-03-15 11:05:01 +0100

    Add test for RTLO infinite loop

---
+++ tests/detectors/rtlo/0.8.0/unicode_direction_override.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+contract C {
+    function f() external pure
+    {
+        // RLO PDF
+        /*ok ‮‬*/
+
+        // RLO RLO PDF PDF
+        /*ok ‮‮‬‬*/
+
+        // RLO RLO RLO PDF PDF PDF
+        /*ok ‮‮‮‬‬‬*/
+    }
+}
+// ----
