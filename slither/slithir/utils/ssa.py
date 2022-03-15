c4e5ce8044e9b100229741d8383a4fdefccf1f68
hbrodin <90325907+hbrodin@users.noreply.github.com>
2022-03-15 11:43:27 +0100

    WIP commit

--- slither/slithir/utils/ssa.py
+++ slither/slithir/utils/ssa.py
@@ -1,6 +1,9 @@
 import logging
+from collections import defaultdict
+from contextlib import contextmanager
 from typing import Union
 
+from build.lib.slither.slithir.variables import tuple
 from slither.core.cfg.node import NodeType
 from slither.core.declarations import (
     Contract,
@@ -99,6 +102,347 @@ def transform_slithir_vars_to_ssa(function):
 
 # pylint: disable=too-many-arguments,too-many-locals,too-many-nested-blocks,too-many-statements,too-many-branches
 
+class VarState:
+    def __init__(self):
+        self.index = 0
+        self.instances = []
+
+    def append(self, ir_var):
+        """
+        Adds a new instance/version of an SSA variable to the current working set
+
+        The odd birds here are the TemporaryVariableSSA and ReferenceVariableSSA
+        which already is indexed (in SSA form) and should not be incremented.
+        """
+        # TODO (hbrodin): Is it correct? Are there any occasions where a temporary variable
+        # would need to have multiple versions?
+        if not isinstance(ir_var, (TemporaryVariableSSA, ReferenceVariableSSA)):
+            ir_var.index = self.index
+            self.index += 1
+        self.instances.append(ir_var)
+
+    def instance_count(self):
+        return len(self.instances)
+
+    def keep_instances(self, n):
+        self.instances = self.instances[:n]
+        return self
+
+
+class VarStates:
+    def __init__(self):
+        self._state = defaultdict(VarState)
+
+    def add(self, v):
+        """Adds a new definition of v, creates a new version"""
+        if v is None or isinstance(v, Constant):
+            return None
+
+        ir_var = self._var_to_ir_var(v)
+        if isinstance(ir_var, TemporaryVariableSSA):
+            print(f"Created {ir_var} from {v}")
+        self._state[v].append(ir_var)
+        return ir_var
+
+    def get(self, v):
+        """Returns the current ssa version of the var v
+
+        If no version exists - that is this is is the first time an argument,
+        return or global variable is referenced - a new ssa version is created
+        and returned.
+        """
+        if v is None or isinstance(v, Constant):
+            return v
+        if v not in self._state:
+            return self.add(v)
+        return self._state[v].instances[-1]
+
+    def _var_to_ir_var(self, v):
+        if isinstance(v, LocalVariable):
+            return LocalIRVariable(v)
+        elif isinstance(v, StateVariable):
+            return StateIRVariable(v)
+        elif isinstance(v, TemporaryVariable):
+            return TemporaryVariableSSA(v)
+        elif isinstance(v, TupleVariable):
+            return TupleVariableSSA(v)
+        elif isinstance(v, ReferenceVariable):
+            return ReferenceVariableSSA(v)
+        raise SlithIRError(f"Unknown variable type{v}")
+
+    @contextmanager
+    def new_scope(self):
+        captured_state = {k: vs.instance_count() for (k, vs) in self._state.items()}
+        yield self
+        [self._state[k].keep_instances(v) for (k, v) in captured_state.items()]
+
+def derive_ssa_initial_state(f: Function, state_variables):
+
+    # TODO (hbrodin): Consiter if the state_variables should be separate
+    # e.g. to not reuse state variable indices.
+    # Old version seems to produce a phi for all state vra writes, but
+    # there are cases that are not possible
+    # state_var = 1
+    # state_var = 2
+    # state_var = 3
+    # No function will see state_var being 1 or 2
+    # However, care needs to be taken:
+    # state_var = 1
+    # external_call()
+    # state_var = 2
+    # state_var = 3
+    # Here, state_var ==1 can be seen elsewhere
+    # There are even more complex cases when loops are involved
+
+    vs = VarStates()
+    # TODO (hbrodin): Should it be like this?
+    #for lv in filter(lambda x: x.name, f.parameters + f.returns):
+    #    _add_ir_var(d, lv)
+
+    #for sv in state_variables:
+    #    if is_used_later(f.entry_point, sv):
+    #        vs.add(sv)
+    return vs
+
+
+def ir_to_ssa_form(ir, state):
+    """
+    Produces SSA form IR from IR
+    Args:
+        ir (Operation)
+
+    NOTE: The order of operation is important, lvalues MUST be
+     computed after r-values are computed. If not the r-value
+     version might use the latest def. That would cause this IR
+     var = var + 1
+     to incorrectly become:
+     var_1 = var_1 + 1
+     instead of the correct:
+     var_1 = var_0 + 1
+    """
+
+    # def get_ssa(var):
+    #     if var is None:
+    #         return None
+    #     if isinstance(var, Constant):
+    #         return var
+    #     return _curr_ssa_var(state, var)
+    #
+    # def add_def(lval):
+    #     if lval is None:
+    #         return None
+    #     return _add_ir_var(state, lval)
+    def get_ssa(var):
+        return state.get(var)
+
+    def add_def(lval):
+        return state.add(lval)
+
+    def _get_traversal(values):
+        ret = []
+        for v in values:
+            if isinstance(v, list):
+                v = _get_traversal(v)
+            else:
+                v = state.get(v)
+            ret.append(v)
+        return ret
+
+    def get_arguments(ir):
+        return _get_traversal(ir.arguments)
+
+    def get_rec_values(ir, f):
+        # Use by InitArray and NewArray
+        # Potential recursive array(s)
+        ori_init_values = f(ir)
+
+        return _get_traversal(ori_init_values)
+
+    if isinstance(ir, Assignment):
+        rvalue = get_ssa(ir.rvalue)
+        lvalue = add_def(ir.lvalue)
+        return Assignment(lvalue, rvalue, ir.variable_return_type)
+    if isinstance(ir, Binary):
+        variable_left = get_ssa(ir.variable_left)
+        variable_right = get_ssa(ir.variable_right)
+        lvalue = add_def(ir.lvalue)
+        return Binary(lvalue, variable_left, variable_right, ir.type)
+    if isinstance(ir, CodeSize):
+        value = get_ssa(ir.value)
+        lvalue = add_def(ir.lvalue)
+        return CodeSize(value, lvalue)
+    if isinstance(ir, Condition):
+        return Condition(get_ssa(ir.value))
+    if isinstance(ir, Delete):
+        variable = get_ssa(ir.variable)
+        lvalue = add_def(ir.lvalue)
+        return Delete(lvalue, variable)
+    if isinstance(ir, EventCall):
+        name = ir.name
+        return EventCall(name)
+    if isinstance(ir, HighLevelCall):  # include LibraryCall
+        destination = get_ssa(ir.destination)
+        call_value = get_ssa(ir.call_value)
+        call_gas = get_ssa(ir.call_gas)
+        arguments = get_arguments(ir)
+
+        lvalue = add_def(ir.lvalue)
+        if isinstance(ir, LibraryCall):
+            new_ir = LibraryCall(destination, ir.function_name, ir.nbr_arguments, lvalue, ir.type_call)
+        else:
+            new_ir = HighLevelCall(destination, ir.function_name, ir.nbr_arguments, lvalue, ir.type_call)
+        new_ir.call_id = ir.call_id
+        new_ir.call_value = call_value
+        new_ir.call_gas = call_gas
+        new_ir.arguments = arguments
+        new_ir.function = ir.function
+        return new_ir
+    if isinstance(ir, Index):
+        variable_left = get_ssa(ir.variable_left)
+        variable_right = get_ssa(ir.variable_right)
+        lvalue = add_def(ir.lvalue)
+        return Index(lvalue, variable_left, variable_right, ir.index_type)
+    if isinstance(ir, InitArray):
+        init_values = get_rec_values(ir, lambda x: x.init_values)
+        lvalue = add_def(ir.lvalue)
+        return InitArray(init_values, lvalue)
+    if isinstance(ir, InternalCall):
+        args = get_arguments(ir)
+        lvalue = add_def(ir.lvalue)
+        new_ir = InternalCall(ir.function, ir.nbr_arguments, lvalue, ir.type_call)
+        new_ir.arguments = args
+        return new_ir
+    if isinstance(ir, InternalDynamicCall):
+        function = get_ssa(ir.function)
+        arguments = get_arguments(ir)
+        lvalue = add_def(ir.lvalue)
+        new_ir = InternalDynamicCall(lvalue, function, ir.function_type)
+        new_ir.arguments = arguments
+        return new_ir
+    if isinstance(ir, LowLevelCall):
+        destination = get_ssa(ir.destination)
+        call_value = get_ssa(ir.call_value)
+        call_gas = get_ssa(ir.call_gas)
+        arguments = get_arguments(ir)
+        lvalue = add_def(ir.lvalue)
+        new_ir = LowLevelCall(destination, ir.function_name, ir.nbr_arguments, lvalue, ir.type_call)
+        new_ir.call_id = ir.call_id
+        new_ir.call_value = call_value
+        new_ir.call_gas = call_gas
+        new_ir.arguments = arguments
+        return new_ir
+    if isinstance(ir, Member):
+        variable_left = get_ssa(ir.variable_right)
+        variable_right = get_ssa(ir.variable_right)
+        lvalue = add_def(ir.lvalue)
+        return Member(variable_left, variable_right, lvalue)
+    if isinstance(ir, NewArray):
+        arguments = get_rec_values(ir, lambda x: x.arguments)
+        lvalue = add_def(ir.lvalue)
+        new_ir = NewArray(ir.depth, ir.array_type, lvalue)
+        new_ir.arguments = arguments
+        return new_ir
+    if isinstance(ir, NewElementaryType):
+        arguments = get_arguments(ir)
+        lvalue = add_def(ir.lvalue)
+        new_ir = NewElementaryType(ir.type, lvalue)
+        new_ir.arguments = arguments
+        return new_ir
+    if isinstance(ir, NewContract):
+        arguments = get_arguments(ir)
+        call_value = get_ssa(ir.call_value)
+        call_salt = get_ssa(ir.call_salt)
+        lvalue = add_def(ir.lvalue)
+        new_ir = NewContract(ir.contract_name, lvalue)
+        new_ir.arguments = arguments
+        new_ir.call_value = call_value
+        new_ir.call_salt = call_salt
+        return new_ir
+    if isinstance(ir, NewStructure):
+        arguments = get_arguments(ir)
+        lvalue = add_def(ir.lvalue)
+        new_ir = NewStructure(ir.structure, lvalue)
+        new_ir.arguments = arguments
+        return new_ir
+    if isinstance(ir, Nop):
+        return Nop()
+    if isinstance(ir, Push):
+        array = get_ssa(ir.array)
+        lvalue = add_def(ir.lvalue)
+        return Push(array, lvalue)
+    if isinstance(ir, Return):
+        values = get_rec_values(ir, lambda x: x.values)
+        return Return(values)
+    if isinstance(ir, Send):
+        destination = get_ssa(ir.destination)
+        call_value = get_ssa(ir.call_value)
+        lvalue = add_def(ir.lvalue)
+        return Send(destination, call_value, lvalue)
+    if isinstance(ir, SolidityCall):
+        arguments = get_arguments(ir)
+        lvalue = add_def(ir.lvalue)
+        new_ir = SolidityCall(ir.function, ir.nbr_arguments, lvalue, ir.type_call)
+        new_ir.arguments = arguments
+        return new_ir
+    if isinstance(ir, Transfer):
+        destination = get_ssa(ir.destination)
+        call_value = get_ssa(ir.call_value)
+        return Transfer(destination, call_value)
+    if isinstance(ir, TypeConversion):
+        variable = get_ssa(ir.variable)
+        lvalue = add_def(ir.lvalue)
+        print(f"Conversion of {variable} ({ir.variable}) to {lvalue} ({ir.lvalue})")
+        return TypeConversion(lvalue, variable, ir.type)
+    if isinstance(ir, Unary):
+        rvalue = get_ssa(ir.rvalue)
+        lvalue = add_def(ir.lvalue)
+        return Unary(lvalue, rvalue, ir.type)
+    if isinstance(ir, Unpack):
+        tuple_var = get_ssa(ir.tuple)
+        lvalue = add_def(ir.lvalue)
+        return Unpack(lvalue, tuple_var, ir.index)
+    if isinstance(ir, Length):
+        value = get_ssa(ir.value)
+        lvalue = add_def(ir.lvalue)
+        return Length(value, lvalue)
+
+    raise SlithIRError("Impossible ir copy on {} ({})".format(ir, type(ir)))
+
+
+def ir_nodes_to_ssa(node, parent_state):
+
+    print(f"ENTER {node}")
+    with parent_state.new_scope() as state:
+        # NOTE (hbrodin): Dummy phi-nodes have been placed in a previous step.
+        # This will ensure that the dummy phi-nodes are replace with correctly
+        # #indexed phi-nodes according to current state when visiting block
+        # having the phi-node.
+        for i, n in enumerate(node.irs_ssa):
+            if isinstance(n, Phi):
+                # Replace the dummy phi with a correctly labeled phi
+                lvalue = state.add(n.lvalue.non_ssa_version)
+                new_ir = Phi(lvalue, n.nodes)
+                for ir_var in n.rvalues:
+                    new_ir.rvalues.append(ir_var)
+                node.irs_ssa[i] = new_ir
+
+        for ir in node.irs:
+            assert not isinstance(ir, (Phi, PhiCallback))
+            # if s is a non-phi statement (by design, no phi in non-ssa ir)
+            ssa_irs = ir_to_ssa_form(ir, state)
+            node.add_ssa_ir(ssa_irs)
+
+        for successor in node.sons:
+            for n in successor.irs_ssa:
+                if isinstance(n, Phi):
+                    orig_var = n.lvalue.non_ssa_version
+                    ssa_var = state.get(orig_var)
+                    n.rvalues.append(ssa_var)
+
+        for successor in node.dominator_successors:
+            ir_nodes_to_ssa(successor, state)
+    print(f"LEAVE {node}")
+
 
 def add_ssa_ir(function, all_state_variables_instances):
     """
@@ -111,25 +455,38 @@ def add_ssa_ir(function, all_state_variables_instances):
     if not function.is_implemented:
         return
 
-    init_definition = {}
-    for v in function.parameters:
-        if v.name:
-            init_definition[v.name] = (v, function.entry_point)
-            function.entry_point.add_ssa_ir(Phi(LocalIRVariable(v), set()))
-
-    for v in function.returns:
-        if v.name:
-            init_definition[v.name] = (v, function.entry_point)
+    # init_definition = {}
+    # for v in function.parameters:
+    #     if v.name:
+    #         init_definition[v.name] = (v, function.entry_point)
+    #         function.entry_point.add_ssa_ir(Phi(LocalIRVariable(v), set()))
+    #
+    # for v in function.returns:
+    #     if v.name:
+    #         init_definition[v.name] = (v, function.entry_point)
 
     # We only add phi function for state variable at entry node if
     # The state variable is used
     # And if the state variables is written in another function (otherwise its stay at index 0)
-    for (_, variable_instance) in all_state_variables_instances.items():
-        if is_used_later(function.entry_point, variable_instance):
+    #for (_, variable_instance) in all_state_variables_instances.items():
+        #if is_used_later(function.entry_point, variable_instance):
             # rvalues are fixed in solc_parsing.declaration.function
-            function.entry_point.add_ssa_ir(Phi(StateIRVariable(variable_instance), set()))
+            #function.entry_point.add_ssa_ir(Phi(StateIRVariable(variable_instance), set()))
 
-    add_phi_origins(function.nodes)
+    # Create the initial set of numbered variables
+    # TODO (hbrodin): Add the storage part from below as well
+    vars = derive_ssa_initial_state(function, all_state_variables_instances.values())
+
+    # Adding phi-nodes based on control flow of function
+    # TODO (hbrodin): add phi nodes after calls (.. maybe later when we have the calls??)
+    add_phi_origins(function.nodes, vars)
+
+    # Transform IR to SSA ir by cloning IR nodes and add version info. This will also
+    # append Phi-nodes after external calls (for any state variable currently used).
+    ir_nodes_to_ssa(function.entry_point, vars)
+
+
+    return
 
     init_local_variables_instances = {}
     for v in function.parameters:
@@ -549,7 +906,7 @@ def _find_var_def(node, var: Union[LocalVariable, StateVariable]):
         node = node.immediate_dominator
 
 
-def add_phi_origins(nodes):
+def add_phi_origins(nodes, vars):
     """Insert Phi-nodes where needed"""
     # Phase 1 place dummy phi nodes
     workset = set()
@@ -593,6 +950,8 @@ def add_phi_origins(nodes):
             #   continue
             nodes_real_origin = set(map(lambda n, var=variable: _find_var_def(n, var), source_nodes))
             node.add_ssa_ir(Phi(LocalIRVariable(variable), nodes_real_origin))
+            #node.add_ssa_ir(Phi(variable, nodes_real_origin))
+            #node.add_ssa_ir(Phi(_add_ir_var(vars, variable), set()))
         for (variable, source_nodes) in node.phi_origins_state_variables.values():
             if len(source_nodes) < 2:
                 # TODO (hbrodin): How do we report errors/inconsistencies?
@@ -604,6 +963,8 @@ def add_phi_origins(nodes):
             #    continue
             nodes_real_origin = set(map(lambda n, var=variable: _find_var_def(n, var), source_nodes))
             node.add_ssa_ir(Phi(StateIRVariable(variable), nodes_real_origin))
+            #node.add_ssa_ir(Phi(variable, nodes_real_origin))
+            #node.add_ssa_ir(Phi(_add_ir_var(vars, variable), set()))
 
 
 
@@ -883,6 +1244,5 @@ def copy_ir(ir, *instances):
         return Length(value, lvalue)
 
     raise SlithIRError("Impossible ir copy on {} ({})".format(ir, type(ir)))
-
 
 # endregion
