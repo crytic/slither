6a4b97b7a760d3e56963a49b46756698c088497d
Tadashi <tadasho@gmail.com>
2022-04-01 21:37:47 +0200

    Improved tests

--- tests/detectors/external-function/0.6.11/external_function_3.sol
+++ tests/detectors/external-function/0.6.11/external_function_3.sol
@@ -1,7 +1,10 @@
 contract Test {
 
-    function test(bytes memory x) public {
-        
-    }
+    function good(bytes calldata x) external {}
+    function good2() public {}
+
+    function good3(uint256 x, uint256 y) public {}
+
+    function good4(uint256[] calldata x, string calldata y) external {}
 
 }
