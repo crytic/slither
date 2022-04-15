f295bf35f83cb8153d871566fa6792eeffb31fe5
Tadashi <tadasho@gmail.com>
2022-04-15 20:59:12 +0200

    Increased tests coverage

--- tests/detectors/external-function/0.4.25/external_function_3.sol
+++ tests/detectors/external-function/0.4.25/external_function_3.sol
@@ -2,9 +2,7 @@ contract Test {
 
     function good(bytes x) external {}
     function good2() public {}
-
     function good3(uint256 x, uint256 y) public {}
-
     function good4(uint256[] x, string y) external {}
 
 }
