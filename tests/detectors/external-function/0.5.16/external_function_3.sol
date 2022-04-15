d15641233d9dfbb191e9b576365cb7d70f75d58b
Tadashi <tadasho@gmail.com>
2022-04-15 20:59:12 +0200

    Increased tests coverage

--- tests/detectors/external-function/0.5.16/external_function_3.sol
+++ tests/detectors/external-function/0.5.16/external_function_3.sol
@@ -1,10 +1,16 @@
+pragma experimental ABIEncoderV2;
+
 contract Test {
+
+    struct testStruct {
+        uint256 id;
+        string name;
+    }
 
     function good(bytes calldata x) external {}
     function good2() public {}
-
     function good3(uint256 x, uint256 y) public {}
-
     function good4(uint256[] calldata x, string calldata y) external {}
+    function good5(testStruct calldata x) external {}
 
 }
