24a503a1af035a5437dbd18d097fdaca6fe4e513
Josselin <josselin@trailofbits.com>
2022-04-21 14:10:03 +0200

    Minor

--- tests/slithir/test_ternary_expressions.py
+++ tests/slithir/test_ternary_expressions.py
@@ -3,8 +3,8 @@ from slither.core.cfg.node import NodeType
 from slither.slithir.operations import Assignment
 from slither.core.expressions import AssignmentOperation, TupleExpression
 
-
-def test_ternary_conversions():
+# pylint: disable=too-many-nested-blocks
+def test_ternary_conversions() -> None:
     """This tests that true and false sons define the same number of variables that the father node declares"""
     slither = Slither("./tests/slithir/ternary_expressions.sol")
     for contract in slither.contracts:
@@ -15,17 +15,18 @@ def test_ternary_conversions():
                     vars_assigned = 0
 
                     # Iterate over true and false son
-                    for node in node.sons:
+                    for inner_node in node.sons:
                         # Count all variables declared
-                        if isinstance(node.expression, AssignmentOperation):
-                            var_expr = node.expression.expression_left
+                        expression = inner_node.expression
+                        if isinstance(expression, AssignmentOperation):
+                            var_expr = expression.expression_left
                             # Only tuples declare more than one var
                             if isinstance(var_expr, TupleExpression):
                                 vars_declared += len(var_expr.expressions)
                             else:
                                 vars_declared += 1
 
-                        for ir in node.irs:
+                        for ir in inner_node.irs:
                             # Count all variables defined
                             if isinstance(ir, Assignment):
                                 vars_assigned += 1
