2e5aa1a20e88b548698fd2959a215dea6b3f08c0
Tadashi <tadasho@gmail.com>
2022-04-27 00:40:51 +0200

    Added "bad" test cases

--- tests/detectors/external-function/0.4.25/external_function_3.sol
+++ tests/detectors/external-function/0.4.25/external_function_3.sol
@@ -4,5 +4,8 @@ contract Test {
     function good2() public {}
     function good3(uint256 x, uint256 y) public {}
     function good4(uint256[] x, string y) external {}
+    function bad(bytes memory x) public {}
+    function bad2(uint256[] memory x) public {}
+    function bad3(string memory x) public {}
 
 }
--- tests/detectors/external-function/0.7.6/external_function_3.sol
+++ tests/detectors/external-function/0.7.6/external_function_3.sol
@@ -12,5 +12,8 @@ contract Test {
     function good3(uint256 x, uint256 y) public {}
     function good4(uint256[] calldata x, string calldata y) external {}
     function good5(testStruct calldata x) external {}
-
+    function bad(bytes memory x) public {}
+    function bad2(uint256[] memory x) public {}
+    function bad3(testStruct memory x) public {}
+    function bad4(string memory x) public {}
 }
