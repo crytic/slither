793c4c4ee4580f2df91b0a7778691b0b1c192999
alpharush <0xalpharush@protonmail.com>
2022-06-24 14:31:30 -0500

    add failing test cases for user defined types

---
+++ tests/ast-parsing/user_defined_value_type/argument.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.9;
+
+type Fix is int192;
+
+
+contract FixLib {
+    function div(uint256 numerator, uint256 divisor) public pure returns (uint256) {
+        return numerator / divisor;
+    }
+    function works(Fix x) external pure returns (uint256) {
+        uint256 y = uint192(Fix.unwrap(x));
+        return div(y, 1e18);
+    }
+    function broken(Fix x) external pure returns (uint256) {
+        return div(uint192(Fix.unwrap(x)), 1e18);
+    }
+}
