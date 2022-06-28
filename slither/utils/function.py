882fc95756e876f93c4248dd6050c16d6a5a5c41
alpharush <0xalpharush@protonmail.com>
2022-06-28 12:30:00 -0500

    replace pysha3 with pycryptodome

--- slither/utils/function.py
+++ slither/utils/function.py
@@ -1,4 +1,4 @@
-import sha3
+from Crypto.Hash import keccak
 
 
 def get_function_id(sig: str) -> int:
@@ -9,6 +9,6 @@ def get_function_id(sig: str) -> int:
     Return:
         (int)
     """
-    s = sha3.keccak_256()
-    s.update(sig.encode("utf-8"))
-    return int("0x" + s.hexdigest()[:8], 16)
+    hash = keccak.new(digest_bits=256)
+    hash.update(sig.encode("utf-8"))
+    return int("0x" + hash.hexdigest()[:8], 16)
