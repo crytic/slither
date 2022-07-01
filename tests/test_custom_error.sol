b121586119092534b14de4076e65c9ca43338829
alpharush <0xalpharush@protonmail.com>
2022-07-01 18:31:22 -0500

    mark where parsing is incorrect

--- tests/test_custom_error.sol
+++ tests/test_custom_error.sol
@@ -7,9 +7,8 @@ contract A {
     
     function g() private {
       bool something = h();
-      if (something) {
-        revert E(this);
-      }
+
+      revert E(this);
     }
 
     function h() private returns (bool something) {
@@ -22,13 +21,15 @@ interface I {
   error SomethingSomething(Enum e);
 }
 
-abstract contract A2 is I {
-}
+// abstract contract A2 is I {
+// }
 
-contract B is A2 {
+// contract B is A2 {
   
-  function f() external {
-    revert SomethingSomething(Enum.ONE);
-  }
-}
+//   function f() external {
+//     revert SomethingSomething(Enum.ONE);
+//   }
+// }
+//{'argumentTypes': [{'typeIdentifier': 't_contract$_A_$32', 'typeString': 'contract A'}], 'id': 20, 'name': 'E', 'nodeType': 'Identifier', 'overloadedDeclarations': [], 'referencedDeclaration': 5, 'src': '159:1:0', 'typeDescriptions': {'typeIdentifier': 't_function_error_pure$_t_contract$_A_$32_$returns$__$', 'typeString': 'function (contract A) pure'}}
+//{'id': 22, 'name': 'this', 'nodeType': 'Identifier', 'overloadedDeclarations': [], 'referencedDeclaration': -28, 'src': '175:4:0', 'typeDescriptions': {'typeIdentifier': 't_contract$_A_$34', 'typeString': 'contract A'}}
 
