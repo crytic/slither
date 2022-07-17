e178b37fc9cd6bb052672b1027e8fafb2cbeb747
noxx <noxx3xxon@gmail.com>
2022-07-17 21:06:27 +0100

    remove crytic-export

--- crytic-export/etherscan-contracts/0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8-UniswapV3Pool/contracts/libraries/FixedPoint128.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.4.0;
-
-/// @title FixedPoint128
-/// @notice A library for handling binary fixed point numbers, see https://en.wikipedia.org/wiki/Q_(number_format)
-library FixedPoint128 {
-    uint256 internal constant Q128 = 0x100000000000000000000000000000000;
-}
