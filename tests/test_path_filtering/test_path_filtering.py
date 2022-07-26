e2afe85b2a5722edd1c11fee2c75f8540101d358
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-07-26 15:25:44 +0200

    refactor and remove CI tests
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- tests/test_path_filtering/test_path_filtering.py
+++ tests/test_path_filtering/test_path_filtering.py
@@ -1,6 +1,4 @@
 from slither import Slither
-from tests.test_features import _run_all_detectors
 
 def test_path_filtering():
-    slither = Slither("./tests/test_path_filtering/test_path_filtering.py")
-    _run_all_detectors(slither)
+    return Slither("./tests/test_path_filtering/test_path_filtering.sol", filter_paths=["filtering_paths/libs", "filtering_paths/src/ReentrancyMock.sol"])
