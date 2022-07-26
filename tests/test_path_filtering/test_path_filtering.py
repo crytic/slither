36875c74125003bccda191a85f6f267344451d90
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-07-26 17:55:37 +0200

    using bash scripts now
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- tests/test_path_filtering/test_path_filtering.py
+++
@@ -1,4 +0,0 @@
-from slither import Slither
-
-def test_path_filtering():
-    return Slither("./tests/test_path_filtering/test_path_filtering.sol", filter_paths=["filtering_paths/libs", "filtering_paths/src/ReentrancyMock.sol"])
