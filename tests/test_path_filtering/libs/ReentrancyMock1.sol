cf3d1c5365e3fc42f77f447b101ed51c73b48d9c
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-07-26 12:28:53 +0200

    solc version 0.8.0
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- tests/test_path_filtering/libs/ReentrancyMock1.sol
+++ tests/test_path_filtering/libs/ReentrancyMock1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.0;
 
 contract ReentrancyMock1 {
     mapping(address => uint256) public userBalances1;
--- tests/test_path_filtering/test_path_filtering.sol
+++ tests/test_path_filtering/test_path_filtering.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.0;
 
 import "./src/ReentrancyMock.sol";
 import "./libs/ReentrancyMock1.sol";
