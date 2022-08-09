8f407e369f651bd9ca7e2d30bd5bd129950f1b5f
Guy Levin <33753158+guywhataguy@users.noreply.github.com>
2022-08-09 12:41:36 -0400

    added wiki skeleton for var order optimizing

---
+++ slither/detectors/variables/optimize_variable_order.py
@@ -0,0 +1,49 @@
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+
+
+class OtimizeVariableOrder(AbstractDetector):
+    """
+    Documentation
+    """
+
+    ARGUMENT = "optimize_var_order"
+    HELP = "Find space optimizations from reordering variables"
+    IMPACT = DetectorClassification.HIGHINFORMATIONAL
+    CONFIDENCE = DetectorClassification.HIGH
+
+    WIKI = "https://github.com/trailofbits/slither/wiki/Optimizing-variable-order-detector"
+
+    WIKI_TITLE = "Optimal Variable Order Detector"
+    WIKI_DESCRIPTION = (
+        "Detect optimizations in the variable order of structs to decrease the number of slots used"
+    )
+    WIKI_EXPLOIT_SCENARIO = """
+```solidity
+    struct original {
+        uint128 a
+        uint256 b
+        uint128 c
+    }
+```
+A struct is declared with an unoptimized variable order. The `original` struct will take 3 stroage slots because each variable will have its own slot."""
+    WIKI_RECOMMENDATION = """
+```solidity
+    struct optimized {
+        uint128 a
+        uint128 c
+        uint256 b
+    }
+```
+The struct's variables are reordered to take advantage of Solidity's variable packing. The struct's variables `a` and `b` will both be packed into the same slot, resulting in the `optimized` struct only taking 2 storage slots."""
+    
+    BITS_PER_SLOT = 256
+
+    def _detect(self):
+
+        for contract in self.compilation_unit.contracts_derived:
+            pass
+            
+        info = ["This is an example"]
+        res = self.generate_result(info)
+
+        return [res]
