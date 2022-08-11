0e172170efd1c920c313c0a2ed52f7de67d3faee
Guy Levin <33753158+guywhataguy@users.noreply.github.com>
2022-08-11 17:56:17 -0400

    added basic tests

---
+++ tests/detectors/optimize-var-order/find_optimized_var_order.sol
@@ -0,0 +1,14 @@
+
+contract C{
+    struct findme {
+        uint256 a;
+        uint8 small1;
+        uint256 b;
+        uint8 small2;
+    }
+
+    function sayHelloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+
+}
--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -1463,6 +1463,31 @@ ALL_TEST_OBJECTS = [
         "arbitrary_send_erc20_permit.sol",
         "0.8.0",
     ),
+    Test(
+        all_detectors.OptimizeVariableOrder,
+        "find_optimized_var_order.sol",
+        "0.4.25",
+    ),
+    Test(
+        all_detectors.OptimizeVariableOrder,
+        "find_optimized_var_order.sol",
+        "0.5.16",
+    ),
+    Test(
+        all_detectors.OptimizeVariableOrder,
+        "find_optimized_var_order.sol",
+        "0.6.11",
+    ),
+    Test(
+        all_detectors.OptimizeVariableOrder,
+        "find_optimized_var_order.sol",
+        "0.7.6",
+    ),
+    Test(
+        all_detectors.OptimizeVariableOrder,
+        "find_optimized_var_order.sol",
+        "0.8.0",
+    ),
 ]
 
 
