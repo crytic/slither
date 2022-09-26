b1ed6b3ad8d472ce872a85485fd06e0037dd63da
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-09-26 16:21:47 +0200

    use solc 0.8.0 for path filtering test
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- scripts/ci_test_path_filtering.sh
+++ scripts/ci_test_path_filtering.sh
@@ -2,6 +2,7 @@
 
 ### Test path filtering across POSIX and Windows
 
+solc-select use 0.8.0
 slither "tests/test_path_filtering/test_path_filtering.sol" --config "tests/test_path_filtering/slither.config.json" > "output.txt" 2>&1
 
 if ! grep -q "0 result(s) found" "output.txt"
