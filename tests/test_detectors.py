d4feb30a500b2100222b8cc7d28755c89a456ac1
xternet <pm@xternet.dev>
2022-10-04 22:14:30 +0200

    new detector: Omitted Return Variables

--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -1553,6 +1553,11 @@ ALL_TEST_OBJECTS = [
         "permit_domain_state_var_collision.sol",
         "0.8.0",
     ),
+    Test(
+        all_detectors.OmittedReturnVariables,
+        "omitted_return_variables.sol",
+        "0.8.7",
+    ),
 ]
 
 
