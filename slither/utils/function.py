44a45e75a4036fa51c076f20d7fd2c04168156f3
alpharush <0xalpharush@protonmail.com>
2022-11-06 20:40:43 -0600

    fix "redefine builtin" lint error

--- slither/utils/function.py
+++ slither/utils/function.py
@@ -9,6 +9,6 @@ def get_function_id(sig: str) -> int:
     Return:
         (int)
     """
-    hash = keccak.new(digest_bits=256)
-    hash.update(sig.encode("utf-8"))
-    return int("0x" + hash.hexdigest()[:8], 16)
+    digest = keccak.new(digest_bits=256)
+    digest.update(sig.encode("utf-8"))
+    return int("0x" + digest.hexdigest()[:8], 16)
