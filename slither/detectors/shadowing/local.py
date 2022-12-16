841f460c33c1f9afa5f8e0093631663da15b061d
alpharush <0xalpharush@protonmail.com>
2022-12-16 11:02:52 -0600

    detect local shadowing of named returns vars

--- slither/detectors/shadowing/local.py
+++ slither/detectors/shadowing/local.py
@@ -49,6 +49,7 @@ contract Bug {
     OVERSHADOWED_MODIFIER = "modifier"
     OVERSHADOWED_STATE_VARIABLE = "state variable"
     OVERSHADOWED_EVENT = "event"
+    OVERSHADOWED_RETURN_VARIABLE = "return variable"
 
     def detect_shadowing_definitions(self, contract):  # pylint: disable=too-many-branches
         """Detects if functions, access modifiers, events, state variables, and local variables are named after
@@ -86,6 +87,15 @@ contract Bug {
                             overshadowed.append(
                                 (self.OVERSHADOWED_STATE_VARIABLE, scope_state_variable)
                             )
+                    # Check named return variables
+                    for named_return in function.returns:
+                        # Shadowed local delcarations in the same function will have "_scope_" in their name.
+                        # See `FunctionSolc._add_local_variable`
+                        if (
+                            "_scope_" in variable.name
+                            and variable.name.split("_scope_")[0] == named_return.name
+                        ):
+                            overshadowed.append((self.OVERSHADOWED_RETURN_VARIABLE, named_return))
 
                 # If we have found any overshadowed objects, we'll want to add it to our result list.
                 if overshadowed:
