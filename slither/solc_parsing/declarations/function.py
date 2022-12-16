08faa901fc427ad458e8a382c2ad3ec024697a8f
Kevin Clancy <kevin.clancy@certik.com>
2022-12-16 14:29:46 -0800

    fix: record 'referencedDeclaration' of identifiers in destructuring initializer AST (#15)
    https://github.com/CertiKProject/slither-task/issues/191

--- slither/solc_parsing/declarations/function.py
+++ slither/solc_parsing/declarations/function.py
@@ -770,6 +770,7 @@ class FunctionSolc(CallerContextExpression):
                     for v in variables:
                         identifier = {
                             "nodeType": "Identifier",
+                            "referencedDeclaration": v["id"],
                             "src": v["src"],
                             "name": v["name"],
                             "typeDescriptions": {"typeString": v["typeDescriptions"]["typeString"]},
