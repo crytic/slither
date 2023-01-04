23d77e23fd0b7d89504d0b6ff291a0b50f370101
pavan-nambi <pavannambi999@gmail.com>
2023-01-04 19:07:55 +0530

    refractor(ver-in-path_filterin)

--- scripts/ci_test_path_filtering.sh
+++ scripts/ci_test_path_filtering.sh
@@ -2,7 +2,7 @@
 
 ### Test path filtering across POSIX and Windows
 
-solc-select use 0.8.0
+solc-select use 0.5.0
 slither "tests/test_path_filtering/test_path_filtering.sol" --config "tests/test_path_filtering/slither.config.json" > "output.txt" 2>&1
 
 if ! grep -q "0 result(s) found" "output.txt"
