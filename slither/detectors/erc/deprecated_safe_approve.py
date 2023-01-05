3d128ce355209faed99dc8d27d34649755b83ee5
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-01-05 08:48:23 -0300

    Fix wording

--- slither/detectors/erc/deprecated_safe_approve.py
+++ slither/detectors/erc/deprecated_safe_approve.py
@@ -22,7 +22,7 @@ class DeprecatedSafeApprove(AbstractDetector):
     )
 
     WIKI_TITLE = "Deprecated safeApprove usage"
-    WIKI_DESCRIPTION = "The purpose of this `safeApprove` is to check that a user is either setting their allowance to zero, or setting it from zero. This is so that the user doesn't try to re-set their allowance from one non-zero number to another non-zero number where they can get front-run by the approved address using their allowance in-between those two transactions. However, this function doesn't actually solve the problem as the approval will still pass if the sandwich attack uses the rest of the remaining allowance. Such behavior lends itself to bugs, unnecessary gas usage, and a false sense of security. Because of that, `safeApprove` has been deprecated."
+    WIKI_DESCRIPTION = "The purpose of `safeApprove` is to check that a user is either setting their allowance to zero, or setting it from zero. This is so that the user doesn't try to re-set their allowance from one non-zero number to another non-zero number where they can get front-run by the approved address using their allowance in-between those two transactions. However, this function doesn't actually solve the problem as the approval will still pass if the sandwich attack uses the rest of the remaining allowance. Such behavior lends itself to bugs, unnecessary gas usage, and a false sense of security. Because of that, `safeApprove` has been deprecated."
 
     # region wiki_exploit_scenario
     WIKI_EXPLOIT_SCENARIO = """
