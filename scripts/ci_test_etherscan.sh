2109b5fad980b98315348bf4f47c01e2077e5b2b
Emilio López <emilio.lopez@trailofbits.com>
2023-01-05 11:11:07 -0300

    ci: etherscan: fix test check

--- scripts/ci_test_etherscan.sh
+++ scripts/ci_test_etherscan.sh
@@ -6,7 +6,7 @@ mkdir etherscan
 cd etherscan || exit 255
 
 echo "::group::Etherscan mainnet"
-if ! slither 0x7F37f78cBD74481E593F9C737776F7113d76B315 --etherscan-apikey "$GITHUB_ETHERSCAN"; then
+if ! slither 0x7F37f78cBD74481E593F9C737776F7113d76B315 --etherscan-apikey "$GITHUB_ETHERSCAN" --no-fail-pedantic; then
     echo "Etherscan mainnet test failed"
     exit 1
 fi
@@ -18,7 +18,7 @@ if [ "$GITHUB_ETHERSCAN" = "" ]; then
 fi
 
 echo "::group::Etherscan rinkeby"
-if ! slither rinkeby:0xFe05820C5A92D9bc906D4A46F662dbeba794d3b7 --etherscan-apikey "$GITHUB_ETHERSCAN"; then
+if ! slither rinkeby:0xFe05820C5A92D9bc906D4A46F662dbeba794d3b7 --etherscan-apikey "$GITHUB_ETHERSCAN" --no-fail-pedantic; then
     echo "Etherscan rinkeby test failed"
     exit 1
 fi
