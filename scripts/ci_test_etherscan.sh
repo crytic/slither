87555778da647dd8ab5d050c5c33f10430078af9
Emilio López <emilio.lopez@trailofbits.com>
2023-01-05 10:31:49 -0300

    ci: etherscan: sleep briefly when the API key is not available
    Etherscan performs a 1/5s rate limit when using no API key, so this
    adds a small random sleep to avoid the second test failing if it
    executes fast enough.

--- scripts/ci_test_etherscan.sh
+++ scripts/ci_test_etherscan.sh
@@ -12,6 +12,11 @@ if ! slither 0x7F37f78cBD74481E593F9C737776F7113d76B315 --etherscan-apikey "$GIT
 fi
 echo "::endgroup::"
 
+# Perform a small sleep when API key is not available (e.g. on PR CI from external contributor)
+if [ "$GITHUB_ETHERSCAN" = "" ];
+    sleep $[ ( $RANDOM % 5 )  + 1 ]s
+fi
+
 echo "::group::Etherscan rinkeby"
 if ! slither rinkeby:0xFe05820C5A92D9bc906D4A46F662dbeba794d3b7 --etherscan-apikey "$GITHUB_ETHERSCAN"; then
     echo "Etherscan rinkeby test failed"
