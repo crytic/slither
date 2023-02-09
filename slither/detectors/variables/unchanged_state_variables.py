8475083800de0e5b3c73ad0820af0c41e3f47c5a
alpharush <0xalpharush@protonmail.com>
2023-02-09 11:28:16 -0600

    fix

--- slither/detectors/variables/unchanged_state_variables.py
+++ slither/detectors/variables/unchanged_state_variables.py
@@ -118,7 +118,7 @@ class UnchangedStateVariables:
                         self.constant_candidates.append(v)
 
                     elif (
-                        not v.is_dynamic
+                        not v.type.is_dynamic
                         and version.parse(self.compilation_unit.solc_version)
                         >= version.parse("0.6.5")
                         and (v in constructor_variables_written or v in variables_initialized)
