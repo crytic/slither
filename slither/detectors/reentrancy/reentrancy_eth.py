20057f20de4292cf6eecd3a5ae864e1084d46b85
S.Sidarth <38394431+sidarth16@users.noreply.github.com>
2023-03-03 14:11:39 +0530

    Update reentrancy_eth.py
    msg.sender

--- slither/detectors/reentrancy/reentrancy_eth.py
+++ slither/detectors/reentrancy/reentrancy_eth.py
@@ -38,7 +38,7 @@ Do not report reentrancies that don't involve Ether (see `reentrancy-no-eth`)"""
 ```solidity
     function withdrawBalance(){
         // send userBalance[msg.sender] Ether to msg.sender
-        // if mgs.sender is a contract, it will call its fallback function
+        // if msg.sender is a contract, it will call its fallback function
         if( ! (msg.sender.call.value(userBalance[msg.sender])() ) ){
             throw;
         }
