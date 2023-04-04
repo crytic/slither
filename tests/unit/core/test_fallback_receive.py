92d3e4ff210bcee97c96a0f77072270ca6840113
alpharush <0xalpharush@protonmail.com>
2023-04-03 23:41:16 -0500

    replace new uses of switch_global_version with solc_binary_path

--- tests/unit/core/test_fallback_receive.py
+++ tests/unit/core/test_fallback_receive.py
@@ -7,10 +7,10 @@ from slither.core.declarations.function import FunctionType
 TEST_DATA_DIR = Path(__file__).resolve().parent / "test_data"
 
 
-def test_fallback_receive():
-    solc_select.switch_global_version("0.6.12", always_install=True)
+def test_fallback_receive(solc_binary_path):
+    solc_path = solc_binary_path("0.6.12")
     file = Path(TEST_DATA_DIR, "fallback.sol").as_posix()
-    slither = Slither(file)
+    slither = Slither(file, solc=solc_path)
     fake_fallback = slither.get_contract_from_name("FakeFallback")[0]
     real_fallback = slither.get_contract_from_name("Fallback")[0]
 
