61e512b7e28a15d24d8d78ea5ce51a571c042c12
devtooligan <devtooligan@tooligan.dev>
2023-05-04 17:04:25 -0700

    merge: halstead

--- slither/printers/summary/halstead.py
+++ slither/printers/summary/halstead.py
@@ -10,11 +10,13 @@
     N1 = the total number of operators
     N2 = the total number of operands
 
-    Extended metrics:
+    Extended metrics1:
     n = n1 + n2  # Program vocabulary
     N = N1 + N2  # Program length
     S = n1 * log2(n1) + n2 * log2(n2) # Estimated program length
     V = N * log2(n) # Volume
+
+    Extended metrics2:
     D = (n1 / 2) * (N2 / n2) # Difficulty
     E = D * V # Effort
     T = E / 18 seconds # Time required to program
@@ -27,6 +29,7 @@ from collections import OrderedDict
 from slither.printers.abstract_printer import AbstractPrinter
 from slither.slithir.variables.temporary import TemporaryVariable
 from slither.utils.myprettytable import make_pretty_table
+from slither.utils.upgradeability import encode_ir_for_halstead
 
 
 def compute_halstead(contracts: list) -> tuple:
@@ -42,18 +45,21 @@ def compute_halstead(contracts: list) -> tuple:
 
         core_metrics:
         {"contract1 name": {
-            "n1_unique_operators": n1,
-            "n2_unique_operands": n1,
             "N1_total_operators": N1,
+            "n1_unique_operators": n1,
             "N2_total_operands": N2,
+            "n2_unique_operands": n1,
         }}
 
-        extended_metrics:
+        extended_metrics1:
         {"contract1 name": {
             "n_vocabulary": n1 + n2,
             "N_prog_length": N1 + N2,
             "S_est_length": S,
             "V_volume": V,
+        }}
+        extended_metrics2:
+        {"contract1 name": {
             "D_difficulty": D,
             "E_effort": E,
             "T_time": T,
@@ -62,7 +68,8 @@ def compute_halstead(contracts: list) -> tuple:
 
     """
     core = OrderedDict()
-    extended = OrderedDict()
+    extended1 = OrderedDict()
+    extended2 = OrderedDict()
     all_operators = []
     all_operands = []
     for contract in contracts:
@@ -72,9 +79,9 @@ def compute_halstead(contracts: list) -> tuple:
             for node in func.nodes:
                 for operation in node.irs:
                     # use operation.expression.type to get the unique operator type
-                    # TODO: This is broken. It considers every operation as a unique operator.
-                    operators.append(operation)
-                    all_operators.append(operation)
+                    encoded_operator = encode_ir_for_halstead(operation)
+                    operators.append(encoded_operator)
+                    all_operators.append(encoded_operator)
 
                     # use operation.used to get the operands of the operation ignoring the temporary variables
                     new_operands = [
@@ -82,13 +89,21 @@ def compute_halstead(contracts: list) -> tuple:
                     ]
                     operands.extend(new_operands)
                     all_operands.extend(new_operands)
-        (core[contract.name], extended[contract.name]) = _calculate_metrics(operators, operands)
-    core["ALL CONTRACTS"] = OrderedDict()
-    extended["ALL CONTRACTS"] = OrderedDict()
-    (core["ALL CONTRACTS"], extended["ALL CONTRACTS"]) = _calculate_metrics(
-        all_operators, all_operands
-    )
-    return (core, extended)
+        (
+            core[contract.name],
+            extended1[contract.name],
+            extended2[contract.name],
+        ) = _calculate_metrics(operators, operands)
+    if len(contracts) > 1:
+        core["ALL CONTRACTS"] = OrderedDict()
+        extended1["ALL CONTRACTS"] = OrderedDict()
+        extended2["ALL CONTRACTS"] = OrderedDict()
+        (
+            core["ALL CONTRACTS"],
+            extended1["ALL CONTRACTS"],
+            extended2["ALL CONTRACTS"],
+        ) = _calculate_metrics(all_operators, all_operands)
+    return (core, extended1, extended2)
 
 
 # pylint: disable=too-many-locals
@@ -115,22 +130,24 @@ def _calculate_metrics(operators, operands):
     T = E / 18
     B = (E ** (2 / 3)) / 3000
     core_metrics = {
-        "n1_unique_operators": n1,
-        "n2_unique_operands": n2,
-        "N1_total_operators": N1,
-        "N2_total_operands": N2,
+        "Total Operators": N1,
+        "Unique Operators": n1,
+        "Total Operands": N2,
+        "Unique Operands": n2,
     }
-    extended_metrics = {
-        "n_vocabulary": str(n1 + n2),
-        "N_prog_length": str(N1 + N2),
-        "S_est_length": f"{S:.0f}",
-        "V_volume": f"{V:.0f}",
-        "D_difficulty": f"{D:.0f}",
-        "E_effort": f"{E:.0f}",
-        "T_time": f"{T:.0f}",
-        "B_bugs": f"{B:.3f}",
+    extended_metrics1 = {
+        "Vocabulary": str(n1 + n2),
+        "Program Length": str(N1 + N2),
+        "Estimated Length": f"{S:.0f}",
+        "Volume": f"{V:.0f}",
     }
-    return (core_metrics, extended_metrics)
+    extended_metrics2 = {
+        "Difficulty": f"{D:.0f}",
+        "Effort": f"{E:.0f}",
+        "Time": f"{T:.0f}",
+        "Estimated Bugs": f"{B:.3f}",
+    }
+    return (core_metrics, extended_metrics1, extended_metrics2)
 
 
 class Halstead(AbstractPrinter):
@@ -143,24 +160,30 @@ class Halstead(AbstractPrinter):
         if len(self.contracts) == 0:
             return self.generate_output("No contract found")
 
-        core, extended = compute_halstead(self.contracts)
+        core, extended1, extended2 = compute_halstead(self.contracts)
 
         # Core metrics: operations and operands
         txt = "\n\nHalstead complexity core metrics:\n"
         keys = list(core[self.contracts[0].name].keys())
-        table1 = make_pretty_table(["Contract", *keys], core, False)
-        txt += str(table1) + "\n"
+        table_core = make_pretty_table(["Contract", *keys], core, False)
+        txt += str(table_core) + "\n"
 
-        # Extended metrics: volume, difficulty, effort, time, bugs
-        # TODO: should we break this into 2 tables? currently 119 chars wide
-        txt += "\nHalstead complexity extended metrics:\n"
-        keys = list(extended[self.contracts[0].name].keys())
-        table2 = make_pretty_table(["Contract", *keys], extended, False)
-        txt += str(table2) + "\n"
+        # Extended metrics1: vocabulary, program length, estimated length, volume
+        txt += "\nHalstead complexity extended metrics1:\n"
+        keys = list(extended1[self.contracts[0].name].keys())
+        table_extended1 = make_pretty_table(["Contract", *keys], extended1, False)
+        txt += str(table_extended1) + "\n"
+
+        # Extended metrics2: difficulty, effort, time, bugs
+        txt += "\nHalstead complexity extended metrics2:\n"
+        keys = list(extended2[self.contracts[0].name].keys())
+        table_extended2 = make_pretty_table(["Contract", *keys], extended2, False)
+        txt += str(table_extended2) + "\n"
 
         res = self.generate_output(txt)
-        res.add_pretty_table(table1, "Halstead core metrics")
-        res.add_pretty_table(table2, "Halstead extended metrics")
+        res.add_pretty_table(table_core, "Halstead core metrics")
+        res.add_pretty_table(table_extended1, "Halstead extended metrics1")
+        res.add_pretty_table(table_extended2, "Halstead extended metrics2")
         self.info(txt)
 
         return res
--- slither/utils/myprettytable.py
+++ slither/utils/myprettytable.py
@@ -4,17 +4,29 @@ from prettytable import PrettyTable
 
 
 class MyPrettyTable:
-    def __init__(self, field_names: List[str]):
+    def __init__(self, field_names: List[str], pretty_align: bool = True): #TODO: True by default?
         self._field_names = field_names
         self._rows: List = []
+        self._options: Dict = {}
+        if pretty_align:
+            self._options["set_alignment"] = []
+            self._options["set_alignment"] += [(field_names[0], "l")]
+            for field_name in field_names[1:]:
+                self._options["set_alignment"] += [(field_name, "r")]
+        else:
+            self._options["set_alignment"] = []
 
     def add_row(self, row: List[Union[str, List[str]]]) -> None:
         self._rows.append(row)
+
 
     def to_pretty_table(self) -> PrettyTable:
         table = PrettyTable(self._field_names)
         for row in self._rows:
             table.add_row(row)
+        if len(self._options["set_alignment"]):
+            for column_header, value in self._options["set_alignment"]:
+                table.align[column_header] = value
         return table
 
     def to_json(self) -> Dict:
@@ -26,21 +38,39 @@ class MyPrettyTable:
 
 # **Dict to MyPrettyTable utility functions**
 
-
-# Converts a dict to a MyPrettyTable.  Dict keys are the row headers.
-# @param headers str[] of column names
-# @param body dict of row headers with a dict of the values
-# @param totals bool optional add Totals row
-def make_pretty_table(headers: list, body: dict, totals: bool = False) -> MyPrettyTable:
+def make_pretty_table(headers: list, body: dict, totals: bool = False, total_header="TOTAL") -> MyPrettyTable:
+    """
+    Converts a dict to a MyPrettyTable.  Dict keys are the row headers.
+    Args:
+        headers: str[] of column names
+        body: dict of row headers with a dict of the values
+        totals: bool optional add Totals row
+        total_header: str optional if totals is set to True this will override the default "TOTAL" header
+    Returns:
+        MyPrettyTable
+    """
     table = MyPrettyTable(headers)
     for row in body:
         table_row = [row] + [body[row][key] for key in headers[1:]]
         table.add_row(table_row)
     if totals:
-        table.add_row(["Total"] + [sum([body[row][key] for row in body]) for key in headers[1:]])
+        table.add_row(["TOTAL"] + [sum([body[row][key] for row in body]) for key in headers[1:]])
     return table
 
+def make_pretty_table_simple(data: dict, first_column_header, second_column_header="") -> MyPrettyTable:
+    """
+    Converts a dict to a MyPrettyTable.  Dict keys are the row headers.
+    Args:
+        data: dict of row headers with a dict of the values
+        column_header: str of column name for 1st column
+    Returns:
+        MyPrettyTable
+    """
 
+    table = MyPrettyTable([first_column_header, second_column_header])
+    for k, v in data.items():
+        table.add_row([k] + [v])
+    return table
 # takes a dict of dicts and returns a dict of dicts with the keys transposed
 # example:
 # in:
