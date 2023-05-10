2cdc5448acd2f2d4bcc3f78b5c71ef6695eb2472
webthethird <ratzbodell@gmail.com>
2023-05-10 11:09:22 -0500

    Make cross-contract taint optional in `compare`

--- slither/utils/upgradeability.py
+++ slither/utils/upgradeability.py
@@ -90,7 +90,7 @@ class TaintedExternalContract:
 
 # pylint: disable=too-many-locals
 def compare(
-    v1: Contract, v2: Contract
+    v1: Contract, v2: Contract, include_external: bool = False
 ) -> Tuple[
     List[Variable],
     List[Variable],
@@ -185,10 +185,12 @@ def compare(
         elif any(func in written_by for func in new_modified_functions + tainted_functions):
             tainted_variables.append(var)
 
-    # Find all external contracts and functions called by new/modified/tainted functions
-    tainted_contracts = tainted_external_contracts(
-        new_functions + modified_functions + tainted_functions
-    )
+    tainted_contracts = []
+    if include_external:
+        # Find all external contracts and functions called by new/modified/tainted functions
+        tainted_contracts = tainted_external_contracts(
+            new_functions + modified_functions + tainted_functions
+        )
 
     return (
         missing_vars_in_v2,
