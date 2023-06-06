1db7e4ca31f98d4715a8b061e53e0dcddaeb59ff
Kevin Clancy <kevin.clancy@certik.com>
2023-06-06 14:28:11 -0700

    removed extra colon

--- slither/slithir/convert.py
+++ slither/slithir/convert.py
@@ -1584,7 +1584,7 @@ def _convert_to_structure_to_list(return_type: Type) -> List[Type]:
     if isinstance(return_type, (MappingType, ArrayType)):
         return []
 
-    assert isinstance(return_type, (ElementaryType, UserDefinedType, TypeAlias)):
+    assert isinstance(return_type, (ElementaryType, UserDefinedType, TypeAlias))
     return [return_type]
 
 def convert_type_of_high_and_internal_level_call(
