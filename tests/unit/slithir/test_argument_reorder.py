189051c99b3c14f148b35118cc27274fe9ecdb85
Kevin Clancy <kevin.clancy@certik.com>
2023-06-28 10:49:01 -0700

    fixed linting errors

--- tests/unit/slithir/test_argument_reorder.py
+++ tests/unit/slithir/test_argument_reorder.py
@@ -8,9 +8,12 @@ from slither.slithir.variables.constant import Constant
 TEST_DATA_DIR = Path(__file__).resolve().parent / "test_data"
 ARG_REORDER_TEST_ROOT = Path(TEST_DATA_DIR, "argument_reorder")
 
+
 def test_struct_constructor_reorder(solc_binary_path) -> None:
     solc_path = solc_binary_path("0.8.15")
-    slither = Slither(Path(ARG_REORDER_TEST_ROOT, "test_struct_constructor.sol").as_posix(), solc=solc_path)
+    slither = Slither(
+        Path(ARG_REORDER_TEST_ROOT, "test_struct_constructor.sol").as_posix(), solc=solc_path
+    )
 
     operations = slither.contracts[0].functions[0].slithir_operations
     constructor_calls = [x for x in operations if isinstance(x, NewStructure)]
@@ -36,9 +39,12 @@ def test_struct_constructor_reorder(solc_binary_path) -> None:
         and constructor_calls[1].arguments[1].value == 4
     )
 
+
 def test_internal_call_reorder(solc_binary_path) -> None:
     solc_path = solc_binary_path("0.8.15")
-    slither = Slither(Path(ARG_REORDER_TEST_ROOT, "test_internal_call_reorder.sol").as_posix(), solc=solc_path)
+    slither = Slither(
+        Path(ARG_REORDER_TEST_ROOT, "test_internal_call_reorder.sol").as_posix(), solc=solc_path
+    )
 
     operations = slither.contracts[0].functions[1].slithir_operations
     internal_calls = [x for x in operations if isinstance(x, InternalCall)]
@@ -51,10 +57,9 @@ def test_internal_call_reorder(solc_binary_path) -> None:
     )
     assert (
         isinstance(internal_calls[0].arguments[1], Constant)
-        and internal_calls[0].arguments[1].value == True
+        and internal_calls[0].arguments[1].value is True
     )
     assert (
         isinstance(internal_calls[0].arguments[2], Constant)
         and internal_calls[0].arguments[2].value == 5
     )
-
