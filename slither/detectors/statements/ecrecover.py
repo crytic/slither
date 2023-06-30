70e23e9cf8186742a2828b724b8d6d8907cfa375
tuturu-tech <elvis.skozdopolj@trailofbits.com>
2023-06-30 13:03:08 +0200

    added nonce and zero address checks

--- slither/detectors/statements/ecrecover.py
+++ slither/detectors/statements/ecrecover.py
@@ -2,10 +2,27 @@
 Module detecting improper use of ecrecover.
 
 """
-
-from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+from collections import defaultdict
+from typing import DefaultDict, List, Tuple, Union
+from slither.utils.output import Output
+from slither.core.declarations.solidity_variables import SolidityFunction
+from slither.core.cfg.node import Node
+from slither.core.declarations.function import Function
+from slither.core.declarations.function_contract import FunctionContract
+from slither.slithir.operations.operation import Operation
+from slither.slithir.operations.assignment import Assignment
+from slither.core.expressions.expression import Expression
+from slither.core.expressions.call_expression import CallExpression
+from slither.core.solidity_types.elementary_type import ElementaryType
+from slither.core.variables.local_variable import LocalVariable
+from slither.detectors.abstract_detector import (
+    AbstractDetector,
+    DetectorClassification,
+    DETECTOR_INFO,
+)
 from slither.slithir.operations import Binary, BinaryType
 from slither.slithir.operations.solidity_call import SolidityCall
+from slither.analyses.data_dependency.data_dependency import is_tainted
 
 
 class Ecrecover(AbstractDetector):
@@ -52,53 +69,94 @@ contract A {
 First, signature does not contain nonce.   
 Second, there is no verification of ecrecover's return value.   
 """
-    WIKI_RECOMMENDATION = "Check return value of ecrecover and signature contains a nonce"
+    WIKI_RECOMMENDATION = (
+        "Check the return value of ecrecover and ensure that the signed data contains a nonce"
+    )
 
-    def _detect(self):
-
+    def _detect(self) -> List[Output]:
         results = []
 
-        for contract in self.compilation_unit.contracts:
-            for function in contract.functions:
-                for call in function.solidity_calls:
-                    if call.name == "ecrecover(bytes32,uint8,bytes32,bytes32)":
-                        flag = 0
-                        for vari in function._variables:
-                            if "nonce" in vari.lower():
-                                flag = 1
-                        if flag == 0:
-                            info = [
-                                "No nonce check found in ",
-                                function,
-                                "\n",
-                            ]
-                            res = self.generate_result(info)
-                            results.append(res)
-                        flag = 0
+        for contract in self.compilation_unit.contracts_derived:
+            for function in contract.functions_and_modifiers_declared:
+                var_results, nonce_results = _detect_ecrecover(function)
+                for _, var_nodes in var_results:
+                    for var, nodes in var_nodes.items():
+                        info: DETECTOR_INFO = [
+                            var,
+                            " lacks a zero-check on ",
+                            ":\n",
+                        ]
+                        for node in nodes:
+                            info += ["\t\t- ", node, "\n"]
+                        res = self.generate_result(info)
+                        results.append(res)
+                for _, nonce_nodes in nonce_results:
+                    for var, nodes in nonce_nodes.items():
+                        info: DETECTOR_INFO = [
+                            var,
+                            " lacks a nonce on ",
+                            ":\n",
+                        ]
+                        print("NODES", nodes)
+                        for node in nodes:
+                            info += ["\t\t- ", node, "\n"]
+                        res = self.generate_result(info)
+                        results.append(res)
 
-                        find_erecovery = 0
-                        for node in function.nodes:
-
-                            for ir in node.irs:
-                                if find_erecovery == 1:
-                                    if isinstance(ir, Binary):
-                                        if (ir.type == BinaryType.NOT_EQUAL) or (
-                                            ir.type == BinaryType.EQUAL
-                                        ):
-                                            flag = 1
-                                else:
-                                    if (
-                                        isinstance(ir, SolidityCall)
-                                        and ir.function.name
-                                        == "ecrecover(bytes32,uint8,bytes32,bytes32)"
-                                    ):
-                                        find_erecovery = 1
-                        if flag == 0:
-                            info = [
-                                "No return check found in ",
-                                function,
-                                "\n",
-                            ]
-                            res = self.generate_result(info)
-                            results.append(res)
         return results
+
+
+def _zero_address_validation(var: LocalVariable, function: Function) -> bool:
+    for node in function.nodes:
+        if node.contains_if() or node.contains_require_or_assert():
+            for ir in node.irs:
+                expression = str(ir.expression)
+                if isinstance(ir, Binary) and str(var) in expression:
+                    if (
+                        ir.type == BinaryType.NOT_EQUAL or ir.type == BinaryType.EQUAL
+                    ) and "address(0)" in expression:
+                        return True
+    return False
+
+
+def _nonce_validation(function: Function) -> bool:
+    for node in function.nodes:
+        if "nonce" in node.variables_written:
+            return True
+    return False
+
+
+def _detect_ecrecover(
+    function: Function,
+) -> Tuple[
+    List[Tuple[Function, DefaultDict[LocalVariable, List[Node]]]],
+    List[Tuple[Function, DefaultDict[LocalVariable, List[Node]]]],
+]:
+    results = []
+    nonce_results = []
+
+    if SolidityFunction("ecrecover(bytes32,uint8,bytes32,bytes32)") in function.solidity_calls:
+        address_list = []
+        nonce_list = []
+        var_nodes = defaultdict(list)
+        nonce_nodes = defaultdict(list)
+        for node in function.nodes:
+            if SolidityFunction("ecrecover(bytes32,uint8,bytes32,bytes32)") in node.solidity_calls:
+                for var in node.local_variables_written:
+                    if "ecrecover(bytes32,uint8,bytes32,bytes32)" in str(var.expression):
+                        address_list.append(var)
+                        var_nodes[var].append(node)
+            if SolidityFunction("keccak256(bytes)") in node.solidity_calls:
+                for var in node.variables_written:
+                    if "nonce" not in str(var.expression) and "keccak256" in str(var.expression):
+                        if var not in nonce_list:
+                            nonce_list.append(var)
+                        nonce_nodes[var].append(node)
+
+        for var in address_list:
+            if not _zero_address_validation(var, function):
+                results.append((function, var_nodes))
+        for var in nonce_list:
+            if not _nonce_validation(function):
+                nonce_results.append((function, nonce_nodes))
+    return (results, nonce_results)
---
+++ tests/e2e/detectors/test_data/ecrecover/0.8.0/ecrecover.sol
@@ -0,0 +1,90 @@
+contract A {
+    struct Info {
+        uint tokenId;
+        address owner;
+        // uint nonce
+    }
+    uint maxBreed = 5;
+    mapping(uint => mapping(uint => address)) list;
+    mapping(uint => uint) count;
+    mapping(address => uint256) nonces;
+
+    function mint(uint tokenId, address addr) internal {
+        require(count[tokenId] < maxBreed, "");
+        list[tokenId][count[tokenId]] = addr;
+        count[tokenId]++;
+    }
+
+    function bad_noSignerCheck_noNonce(
+        Info calldata info,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        bytes32 hash = keccak256(abi.encode(info));
+        bytes32 data = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+        );
+        address receiver = ecrecover(data, v, r, s);
+        mint(info.tokenId, receiver);
+    }
+
+    function bad_noSignerCheck_withNonce(
+        Info calldata info,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        bytes32 hash = keccak256(abi.encode(info, nonces[msg.sender]++));
+        bytes32 data = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+        );
+        address receiver = ecrecover(data, v, r, s);
+        mint(info.tokenId, receiver);
+    }
+
+    function bad_withRequire_noNonce(
+        Info calldata info,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        bytes32 hash = keccak256(abi.encode(info));
+        bytes32 data = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+        );
+        address receiver = ecrecover(data, v, r, s);
+        require(receiver != address(0), "ECDSA: invalid signature");
+        mint(info.tokenId, receiver);
+    }
+
+    function bad_withIF_noNonce(
+        Info calldata info,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        bytes32 hash = keccak256(abi.encode(info));
+        bytes32 data = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+        );
+        address receiver = ecrecover(data, v, r, s);
+        if (receiver == address(0)) revert();
+        mint(info.tokenId, receiver);
+    }
+
+    function good_withNonceAndSignerCheck(
+        Info calldata info,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        bytes32 hash = keccak256(abi.encode(info, nonces[msg.sender]++));
+        bytes32 data = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+        );
+        address receiver = ecrecover(data, v, r, s);
+        require(receiver != address(0), "ECDSA: invalid signature");
+        mint(info.tokenId, receiver);
+    }
+}
