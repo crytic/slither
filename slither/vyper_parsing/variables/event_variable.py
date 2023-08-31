2dbb912bb72278c5e15047f95bccfb219e310396
alpharush <0xalpharush@protonmail.com>
2023-08-31 13:46:26 -0500

    correctly set indexed attribute for event variables

--- slither/vyper_parsing/variables/event_variable.py
+++ slither/vyper_parsing/variables/event_variable.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 from slither.core.variables.event_variable import EventVariable
 from slither.vyper_parsing.type_parsing import parse_type
-from slither.vyper_parsing.ast.types import AnnAssign
+from slither.vyper_parsing.ast.types import AnnAssign, Call
 
 
 class EventVariableVyper:
@@ -10,7 +10,10 @@ class EventVariableVyper:
         print(variable_data)
         self._variable = variable
         self._variable.name = variable_data.target.id
-        # TODO self._variable.indexed
+        if isinstance(variable_data.annotation, Call) and variable_data.annotation.func.id == "indexed":
+            self._variable.indexed = True 
+        else:
+            self._variable.indexed = False
         self._elem_to_parse = variable_data.annotation
 
     @property
