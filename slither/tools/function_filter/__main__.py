465d5826e80e1dea153091b7622abc83f43a5b34
shortdoom <oskar.milosz.wojciechowski@gmail.com>
2023-11-18 23:11:40 +0100

    chore: minor changes to basic filtering, add state-change flag

--- slither/tools/function_filter/__main__.py
+++ slither/tools/function_filter/__main__.py
@@ -47,36 +47,43 @@ def parse_args() -> Namespace:
         help="If set, filter functions that make internal calls.",
     )
 
-    # parser.add_argument(
-    #     "--state-change", action="store_true", help="If set, filter functions that change state."
-    # )
+    parser.add_argument(
+        "--state-change", action="store_true", help="If set, filter functions that change state."
+    )
 
     cryticparser.init(parser)
 
     return parser.parse_args()
+
 
 def filter_function(function: Function, args) -> bool:
     # Check visibility
     if args.visibility and function.visibility != args.visibility:
         return False
 
-    # Check for modifiers
+    # Check for existence of modifiers
     if args.modifiers:
-        if not function.modifiers():
+        if not function.modifiers:
             return False
 
-    # Check for external calls
+    # Check for existence of external calls
     if args.ext_calls:
-        if not function.high_level_calls():
+        if not function.high_level_calls:
             return False
 
-    # Check for internal calls
+    # Check for existence of internal calls
     if args.int_calls:
-        if not function.internal_calls():
+        if not function.internal_calls:
+            return False
+
+    # Check if function potentially changes state
+    if args.state_change:
+        if not function._view or not function._pure:
             return False
 
     # If none of the conditions have returned False, the function matches all provided criteria
     return True
+
 
 def main() -> None:
     args = parse_args()
@@ -91,35 +98,34 @@ def main() -> None:
 
     for contract in slither.contracts:
         # Scan only target contract's functions (declared and inherited)
-        if contract.name == contract_name:
-            # Find directly inherited contracts
-            contracts_inherited = [
-                parent for parent in contract.immediate_inheritance if not parent.is_interface
-            ]
+        if contract_name:
+            if contract.name == contract_name:
+                # Find directly inherited contracts
+                contracts_inherited = [
+                    parent for parent in contract.immediate_inheritance if not parent.is_interface
+                ]
 
-            # Iterate declared functions
-            for function in contract.functions:
-                if filter_function(function, args):
-                    filter_results.append(function.get_summary())
-
-            # Iterate inherited functions
-            for contracts in contracts_inherited:
-                for function in contracts.functions:
+                # Iterate declared functions
+                for function in contract.functions:
                     if filter_function(function, args):
                         filter_results.append(function.get_summary())
 
-        # Scan everything if no target contract is specified
-        if not contract_name:
+                # Iterate inherited functions
+                for contracts in contracts_inherited:
+                    for function in contracts.functions:
+                        if filter_function(function, args):
+                            filter_results.append(function.get_summary())
+        else:
             for function in contract.functions:
                 if filter_function(function, args):
                     filter_results.append(function.get_summary())
-    
+
     if filter_results:
         for result in filter_results:
             print(result)
     else:
         print("No results found.")
-    
+
 
 if __name__ == "__main__":
     main()
