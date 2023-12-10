d45d1449f42135dc307f7bfb30e2327ef3cf42b8
Talfao <ondrato@gmail.com>
2023-12-10 09:41:24 +0100

    First testing

---
+++ tests/e2e/detectors/test_data/oracle/0.8.20/oracle_data_check1.sol
@@ -0,0 +1,50 @@
+pragma solidity 0.8.20;
+
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    function getRoundData(
+        uint80 _roundId
+    )
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
+contract OracleWithoutChecks {
+    AggregatorV3Interface priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+    }
+
+    function getPriceUSD() external view returns (uint256) {
+        //(uint80 roundID, int256 price, uint256 startedAt, uint256 timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // chainlink price data is 8 decimals for WETH/USD
+        return uint256(price) * 1e10;
+    }
+}
