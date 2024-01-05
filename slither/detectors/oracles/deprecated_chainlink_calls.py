de39bed8eeeb9c598ef2c9e703559fcd9fe4f18e
Talfao <ondrato@gmail.com>
2024-01-05 11:50:25 +0100

    Add deprecated call check

---
+++ slither/detectors/oracles/deprecated_chainlink_calls.py
@@ -0,0 +1,45 @@
+from slither.detectors.abstract_detector import (
+    AbstractDetector,
+    DetectorClassification,
+    DETECTOR_INFO,
+)
+from slither.core.declarations.contract import Contract
+from slither.slithir.operations import HighLevelCall
+
+class DeprecatedChainlinkCalls(AbstractDetector):
+    """
+    Documentation
+    """
+
+    ARGUMENT = "deprecated_chainlink_call"  # slither will launch the detector with slither.py --detect mydetector
+    HELP = "Help printed by slither"
+    IMPACT = DetectorClassification.HIGH
+    CONFIDENCE = DetectorClassification.HIGH
+
+    WIKI = "RUN"
+
+    WIKI_TITLE = "asda"
+    WIKI_DESCRIPTION = "Slot0 is vulnerable to price manipulation as it gets price at the current moment. TWAP should be used instead."
+    WIKI_EXPLOIT_SCENARIO = "asdsad"
+    WIKI_RECOMMENDATION = "asdsad"
+
+    DEPRECATED_CHAINLINK_CALLS = ["getAnswer", "getTimestamp", "latestAnswer", "latestRound", "latestTimestamp"]
+
+    def find_usage_of_deprecated_chainlink_calls(self, contracts : Contract):
+        results = []
+        for contract in contracts:
+            for function in contract.functions:
+                for node in function.nodes:
+                    for ir in node.irs:
+                        if isinstance(ir, HighLevelCall):
+                            if ir.function.name in self.DEPRECATED_CHAINLINK_CALLS:
+                                results.append(f"Deprecated Chainlink call {ir.function.name} found in {node.source_mapping}")
+        return results
+
+    def _detect(self):
+        results = self.find_usage_of_deprecated_chainlink_calls(self.contracts)
+        if len(results) > 0:
+            res = self.generate_result(results)
+            return [res]
+        return []
+
--- tests/e2e/detectors/test_data/oracle/0.8.20/oracle_non_revert.sol
+++ tests/e2e/detectors/test_data/oracle/0.8.20/oracle_non_revert.sol
@@ -1,3 +1,5 @@
+pragma solidity 0.8.20;
+
 // SPDX-License-Identifier: MIT
 
 interface AggregatorV3Interface {
