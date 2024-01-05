a93e35b661a7c74f01e6b4d2dddc9230b1114cc7
Talfao <ondrato@gmail.com>
2024-01-05 09:29:07 +0100

    double internal problem solved

--- slither/detectors/oracles/oracle.py
+++ slither/detectors/oracles/oracle.py
@@ -31,6 +31,7 @@ from slither.analyses.data_dependency.data_dependency import is_tainted, get_dep
 # debugpy.breakpoint()
 # print('break on this line')
 
+
 class Oracle:
     def __init__(self, _contract, _function, _node, _line_of_call, _returned_used_vars, _interface):
         self.contract = _contract
@@ -51,13 +52,15 @@ class Oracle:
         #     "startedAt",
         # ]
 
-class VarInCondition():
+
+class VarInCondition:
     def __init__(self, _var, _nodes):
         self.var = _var
         self.nodes_with_var = _nodes
 
+
 class OracleDetector(AbstractDetector):
- 
+
     # https://github.com/crytic/slither/wiki/Python-API
     # def detect_stale_price(Function):
     ORACLE_CALLS = [
@@ -70,11 +73,14 @@ class OracleDetector(AbstractDetector):
         Detects off-chain oracle contract and VAR
         """
         oracles = []
-        for contract in contracts: 
+        for contract in contracts:
             for function in contract.functions:
                 if function.is_constructor:
                     continue
-                oracle_calls_in_function, oracle_returned_var_indexes, = self.check_chainlink_call(function) 
+                (
+                    oracle_calls_in_function,
+                    oracle_returned_var_indexes,
+                ) = self.check_chainlink_call(function)
                 if oracle_calls_in_function:
                     for node in oracle_calls_in_function:
                         interface = None
@@ -84,32 +90,28 @@ class OracleDetector(AbstractDetector):
                         idxs = []
                         # if referecne_variable_assigned and isinstance(interface, ReferenceVariable):
                         #     break
-                        # if isinstance(interface, ReferenceVariable): 
+                        # if isinstance(interface, ReferenceVariable):
                         #     referecne_variable_assigned = True
                         for idx in oracle_returned_var_indexes:
                             if idx[0] == node:
                                 idxs.append(idx[1])
                         # print(node, interface, contract)
-                        oracle = Oracle(contract, function, node, node.source_mapping.lines[0], idxs, interface)
+                        oracle = Oracle(
+                            contract, function, node, node.source_mapping.lines[0], idxs, interface
+                        )
                         oracles.append(oracle)
         return oracles
-    
+
     def compare_chainlink_call(self, function) -> bool:
         for call in self.ORACLE_CALLS:
             if call in str(function):
                 return True
         return False
-    
-    def _is_instance(self, ir: Operation) -> bool:  # pylint: disable=no-self-use
-        return (
-            isinstance(ir, HighLevelCall)
-            and
-                (
-                    isinstance(ir.function, Function)
-                    and self.compare_chainlink_call(ir.function.name)
-                )
-        )
 
+    def _is_instance(self, ir: Operation) -> bool:  # pylint: disable=no-self-use
+        return isinstance(ir, HighLevelCall) and (
+            isinstance(ir.function, Function) and self.compare_chainlink_call(ir.function.name)
+        )
 
     def check_chainlink_call(self, function: FunctionContract):
         used_returned_vars = []
@@ -131,14 +133,16 @@ class OracleDetector(AbstractDetector):
                 for read in ir.read:
                     remove = (read, ir.index) if isinstance(ir, Unpack) else (read, None)
                     if remove in values_returned:
-                        used_returned_vars.append(remove) # This is saying which element is used based on the index
+                        used_returned_vars.append(
+                            remove
+                        )  # This is saying which element is used based on the index
                         # this is needed to remove the tuple variable when the first time one of its element is used
                         if remove[1] is not None and (remove[0], None) in values_returned:
                             values_returned.remove((remove[0], None))
                         values_returned.remove(remove)
         returned_vars_used_indexes = []
         for (value, index) in used_returned_vars:
-            returned_vars_used_indexes.append((nodes_origin[value].node,index))                 
+            returned_vars_used_indexes.append((nodes_origin[value].node, index))
         return oracle_calls, returned_vars_used_indexes
 
     def get_returned_variables_from_oracle(self, node) -> list:
@@ -149,10 +153,10 @@ class OracleDetector(AbstractDetector):
         for exp in node.variables_written_as_expression:
             for v in exp.expressions:
                 for var in written_vars:
-                    if (str(v) == str(var.name)):
+                    if str(v) == str(var.name):
                         ordered_vars.append(var)
         return ordered_vars
-    
+
     def check_var_condition_match(self, var, node) -> bool:
         for (
             var2
@@ -165,16 +169,15 @@ class OracleDetector(AbstractDetector):
                 return True
         return False
 
-    
     def map_condition_to_var(self, var, function: FunctionContract):
         nodes = []
         for node in function.nodes:
             if node.is_conditional() and self.check_var_condition_match(var, node):
                 nodes.append(node)
         return nodes
-    
+
     def var_in_function(self, var, function: FunctionContract):
-        
+
         return False
 
     def vars_in_conditions(self, oracle: Oracle) -> bool:
@@ -192,18 +195,20 @@ class OracleDetector(AbstractDetector):
             ) or oracle.function.is_reading_in_require_or_assert(
                 var
             ):  # These two functions check if within the function some var is in require/assert of in if statement
-               
+
                 self.nodes_with_var = self.map_condition_to_var(var, oracle.function)
                 for node in self.nodes_with_var:
                     for ir in node.irs:
                         if isinstance(ir, InternalCall):
                             self.investigate_internal_call(ir.function, var)
-                            
+
                 if len(self.nodes_with_var) > 0:
                     vars_in_condition.append(VarInCondition(var, self.nodes_with_var))
                     oracle_vars.append(VarInCondition(var, self.nodes_with_var))
             else:
-                if self.investigate_internal_call(oracle.function, var): #TODO i need to chnge this to check for taint analysis somehow
+                if self.investigate_internal_call(
+                    oracle.function, var
+                ):  # TODO i need to chnge this to check for taint analysis somehow
                     vars_in_condition.append(VarInCondition(var, self.nodes_with_var))
                     oracle_vars.append(VarInCondition(var, self.nodes_with_var))
                 else:
@@ -214,13 +219,18 @@ class OracleDetector(AbstractDetector):
         oracle.vars_not_in_condition = vars_not_in_condition
         oracle.oracle_vars = oracle_vars
 
-
     def map_param_to_var(self, var, function: FunctionContract):
         for param in function.parameters:
             origin_vars = get_dependencies(param, function)
             for var2 in origin_vars:
                 if var2 == var:
                     return param
+
+    def is_internal_call(self, node):
+        for ir in node.irs:
+            if isinstance(ir, InternalCall):
+                return True
+        return False
 
     def investigate_internal_call(self, function: FunctionContract, var) -> bool:
         if function is None:
@@ -229,22 +239,30 @@ class OracleDetector(AbstractDetector):
         original_var_as_param = self.map_param_to_var(var, function)
         if original_var_as_param is None:
             return False
-        
-        
-        if function.is_reading_in_conditional_node(original_var_as_param) or function.is_reading_in_require_or_assert(original_var_as_param):
+
+        if function.is_reading_in_conditional_node(
+            original_var_as_param
+        ) or function.is_reading_in_require_or_assert(original_var_as_param):
+            conditions = []
             for node in function.nodes:
-                if node.is_conditional() and self.check_var_condition_match(original_var_as_param, node):
-                    self.nodes_with_var.append(node)
-            if len(self.nodes_with_var) > 0:
+                if (
+                    node.is_conditional()
+                    and self.check_var_condition_match(original_var_as_param, node)
+                    and not self.is_internal_call(node)
+                ):
+                    conditions.append(node)
+            if len(conditions) > 0:
+                for cond in conditions:
+                    self.nodes_with_var.append(cond)
                 return True
-        else:
-            for node in function.nodes:
-                for ir in node.irs:
-                    if isinstance(ir, InternalCall):
-                        if self.investigate_internal_call(ir.function, var):
-                            return True
-        
-                   
+
+        for node in function.nodes:
+            for ir in node.irs:
+                if isinstance(ir, InternalCall):
+                    if self.investigate_internal_call(ir.function, original_var_as_param):
+                        return True
+        return False
+
         # for functionCalled in function.internal_calls:
         #     if isinstance(functionCalled, FunctionContract):
         #         print("functionCalled", functionCalled)
@@ -262,15 +280,12 @@ class OracleDetector(AbstractDetector):
         #         #             return True
         #         if self.investigate_internal_call(functionCalled, var):
         #             return True
-        return False
 
     def _detect(self):
         info = []
         self.oracles = self.chainlink_oracles(self.contracts)
         for oracle in self.oracles:
-            oracle.oracle_vars = self.get_returned_variables_from_oracle(
-                oracle.node
-            )
+            oracle.oracle_vars = self.get_returned_variables_from_oracle(oracle.node)
             self.vars_in_conditions(oracle)
         # for oracle in oracles:
         #     oracle_vars = self.get_returned_variables_from_oracle(
--- slither/detectors/oracles/oracle_validate_data.py
+++ slither/detectors/oracles/oracle_validate_data.py
@@ -26,13 +26,13 @@ from typing import List
 from slither.slithir.variables.constant import Constant
 
 
-
 class OracleVarType(Enum):
     ROUNDID = 0
     ANSWER = 1
     STARTEDAT = 2
     UPDATEDAT = 3
     ANSWEREDINROUND = 4
+
 
 class OracleDataCheck(OracleDetector):
     """
@@ -51,17 +51,16 @@ class OracleDataCheck(OracleDetector):
     WIKI_EXPLOIT_SCENARIO = "asdsad"
     WIKI_RECOMMENDATION = "asdsad"
 
- 
-
     def check_staleness(self, var: VarInCondition) -> bool:
         if var is None:
             return False
         for node in var.nodes_with_var:
             str_node = str(node)
             # print(str_node)
-            if "block.timestamp" in str_node: #TODO maybe try something like block.timestamp - updatedAt < b
+            if (
+                "block.timestamp" in str_node
+            ):  # TODO maybe try something like block.timestamp - updatedAt < b
                 return True
-                    
 
             # for ir in node.irs:
             #     if isinstance(ir, Binary):
@@ -73,10 +72,10 @@ class OracleDataCheck(OracleDetector):
             #         elif ir.type in (BinaryType.GREATER, BinaryType.GREATER_EQUAL):
             #             pass
         return False
-    
-    
 
-    def check_RoundId(self, var: VarInCondition, var2: VarInCondition) -> bool: # https://solodit.xyz/issues/chainlink-oracle-return-values-are-not-handled-property-halborn-savvy-defi-pdf
+    def check_RoundId(
+        self, var: VarInCondition, var2: VarInCondition
+    ) -> bool:  # https://solodit.xyz/issues/chainlink-oracle-return-values-are-not-handled-property-halborn-savvy-defi-pdf
         if var is None or var2 is None:
             return False
         for node in var.nodes_with_var:
@@ -86,13 +85,13 @@ class OracleDataCheck(OracleDetector):
                         if ir.variable_right == var.var and ir.variable_left == var2.var:
                             return True
                     elif ir.type in (BinaryType.LESS, BinaryType.LESS_EQUAL):
-                        if (ir.variable_right == var2.var and ir.variable_left == var.var):
+                        if ir.variable_right == var2.var and ir.variable_left == var.var:
                             return True
             if self.check_revert(node):
                 return True
-                       
+
         return False
-    
+
     def check_revert(self, node: Node) -> bool:
         for n in node.sons:
             if n.type == NodeType.EXPRESSION:
@@ -110,32 +109,32 @@ class OracleDataCheck(OracleDetector):
                         for value in ir.values:
                             print(value)
 
-                            
-    def check_price(self, var: VarInCondition, oracle: Oracle) -> bool: #TODO I need to divie require or IF
+    def check_price(
+        self, var: VarInCondition, oracle: Oracle
+    ) -> bool:  # TODO I need to divie require or IF
         if var is None:
             return False
-        for node in var.nodes_with_var: #TODO testing
+        for node in var.nodes_with_var:  # TODO testing
             for ir in node.irs:
                 if isinstance(ir, Binary):
                     if isinstance(ir.variable_right, Constant):
                         if ir.type is (BinaryType.GREATER):
-                            if (ir.variable_right.value == 0):
+                            if ir.variable_right.value == 0:
                                 return True
                         elif ir.type is (BinaryType.NOT_EQUAL):
-                            if (ir.variable_right.value == 0):
+                            if ir.variable_right.value == 0:
                                 return True
-                    if isinstance(ir.variable_left, Constant):        
+                    if isinstance(ir.variable_left, Constant):
                         if ir.type is (BinaryType.LESS):
-                            if (ir.variable_left.value == 0):
+                            if ir.variable_left.value == 0:
                                 return True
             if self.check_revert(node):
                 return True
             elif self.return_boolean(node):
                 return True
-                        
 
         return False
-    
+
     def generate_naive_order(self):
         vars_order = {}
         vars_order[OracleVarType.ROUNDID.value] = None
@@ -145,14 +144,11 @@ class OracleDataCheck(OracleDetector):
         vars_order[OracleVarType.ANSWEREDINROUND.value] = None
         return vars_order
 
-
-     
     def find_which_vars_are_used(self, oracle: Oracle):
         vars_order = self.generate_naive_order()
         for i in range(len(oracle.oracle_vars)):
             vars_order[oracle.returned_vars_indexes[i]] = oracle.oracle_vars[i]
         return vars_order
-
 
     def is_needed_to_check_conditions(self, oracle, var):
         if isinstance(var, VarInCondition):
@@ -176,10 +172,8 @@ class OracleDataCheck(OracleDetector):
                                 answer_checked = True
         startedAt_checked = self.check_staleness(startedAt)
         print(answer_checked, startedAt_checked)
-            
+
         return answer_checked and startedAt_checked
-
-
 
     def naive_check(self, oracle: Oracle):
         vars_order = self.find_which_vars_are_used(oracle)
@@ -192,27 +186,39 @@ class OracleDataCheck(OracleDetector):
             #         problems.append("RoundID value is not checked correctly. It was returned by the oracle call in the function {} of contract {}.\n".format( oracle.function, oracle.node.source_mapping))
             if index == OracleVarType.ANSWER.value:
                 if not self.check_price(var, oracle):
-                    problems.append("Price value is not checked correctly. It was returned by the oracle call in the function {} of contract {}.\n".format( oracle.function, oracle.node.source_mapping))
+                    problems.append(
+                        "Price value is not checked correctly. It was returned by the oracle call in the function {} of contract {}.\n".format(
+                            oracle.function, oracle.node.source_mapping
+                        )
+                    )
             elif index == OracleVarType.UPDATEDAT.value:
                 if not self.check_staleness(var):
-                    problems.append("UpdatedAt value is not checked correctly. It was returned by the oracle call in the function {} of contract {}.\n".format( oracle.function, oracle.node.source_mapping))
-            elif index == OracleVarType.STARTEDAT.value and vars_order[OracleVarType.STARTEDAT.value] is not None:
+                    problems.append(
+                        "UpdatedAt value is not checked correctly. It was returned by the oracle call in the function {} of contract {}.\n".format(
+                            oracle.function, oracle.node.source_mapping
+                        )
+                    )
+            elif (
+                index == OracleVarType.STARTEDAT.value
+                and vars_order[OracleVarType.STARTEDAT.value] is not None
+            ):
                 if self.is_sequencer_check(vars_order[OracleVarType.ANSWER.value], var):
-                    problems = [] #TODO send some hook to another detector
+                    problems = []  # TODO send some hook to another detector
                     break
         return problems
-    
+
     def process_not_checked_vars(self):
         result = []
         for oracle in self.oracles:
             if len(oracle.vars_not_in_condition) > 0:
-                result.append("In contract `{}` a function `{}` uses oracle where the values of vars {} are not checked. This can potentially lead to a problem! \n".format(
+                result.append(
+                    "In contract `{}` a function `{}` uses oracle where the values of vars {} are not checked. This can potentially lead to a problem! \n".format(
                         oracle.contract.name,
                         oracle.function.name,
                         [var.name for var in oracle.vars_not_in_condition],
-                    ))
+                    )
+                )
         return result
-
 
     def _detect(self):
         results = []
