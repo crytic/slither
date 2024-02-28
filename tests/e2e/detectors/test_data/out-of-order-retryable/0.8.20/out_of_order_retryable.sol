69d3c257b4285c0935c797ff99f52cb44448564e
alpharush <0xalpharush@protonmail.com>
2024-02-28 17:36:49 -0600

    feat: add out-of-order-retryable ticket detector

---
+++ tests/e2e/detectors/test_data/out-of-order-retryable/0.8.20/out_of_order_retryable.sol
@@ -0,0 +1,82 @@
+interface Y {
+     function createRetryableTicket(
+     address to,
+     uint256 l2CallValue,
+     uint256 maxSubmissionCost,
+     address excessFeeRefundAddress,
+     address callValueRefundAddress,
+     uint256 gasLimit,
+     uint256 maxFeePerGas,
+     bytes calldata data
+     ) external payable returns (uint256);
+}
+
+contract X {
+function good() external {
+     if (true) {
+          Y(msg.sender).createRetryableTicket(
+               address(1),
+               0,
+               0,
+               address(0),
+               address(0),
+               0,
+               0,
+               "");
+     } else {
+        Y(msg.sender).createRetryableTicket(
+               address(2),
+               0,
+               0,
+               address(0),
+               address(0),
+               0,
+               0,
+               "");
+     }
+}
+function bad1() external {
+     if (true) {
+        Y(msg.sender).createRetryableTicket(
+               address(1),
+               0,
+               0,
+               address(0),
+               address(0),
+               0,
+               0,
+               "");
+     }    
+        Y(msg.sender).createRetryableTicket(
+               address(2),
+               0,
+               0,
+               address(0),
+               address(0),
+               0,
+               0,
+               "");
+
+}
+function bad2() external {
+        Y(msg.sender).createRetryableTicket(
+               address(1),
+               0,
+               0,
+               address(0),
+               address(0),
+               0,
+               0,
+               "");
+          
+        Y(msg.sender).createRetryableTicket(
+               address(2),
+               0,
+               0,
+               address(0),
+               address(0),
+               0,
+               0,
+               "");
+}
+}
