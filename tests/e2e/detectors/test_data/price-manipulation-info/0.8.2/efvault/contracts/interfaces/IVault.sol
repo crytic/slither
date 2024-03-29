fa736eb0518bedb22f6175d58f37e32650fb76d3
xueyue <nerbonic@gmail.com>
2024-03-29 14:34:37 +0800

    delete irrelevant code

--- tests/e2e/detectors/test_data/price-manipulation-info/0.8.2/efvault/contracts/interfaces/IVault.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IVault {
-    function deposit(uint256 assets, address receiver) external payable returns (uint256 shares);
-
-    function mint(uint256 amount, address account) external;
-}
