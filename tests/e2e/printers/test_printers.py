114dc2eecaff3262472645f19f961971ee448de5
Alexis <alexis.challande@trailofbits.com>
2024-04-08 11:43:42 +0200

    Fix #2266 where the type of functions when used as RValues was not properly printed.

--- tests/e2e/printers/test_printers.py
+++ tests/e2e/printers/test_printers.py
@@ -7,6 +7,7 @@ from crytic_compile.platform.solc_standard_json import SolcStandardJson
 
 from slither import Slither
 from slither.printers.inheritance.inheritance_graph import PrinterInheritanceGraph
+from slither.printers.summary.slithir import PrinterSlithIR
 
 
 TEST_DATA_DIR = Path(__file__).resolve().parent / "test_data"
@@ -34,3 +35,18 @@ def test_inheritance_printer(solc_binary_path) -> None:
 
     assert counter["B -> A"] == 2
     assert counter["C -> A"] == 1
+
+
+def test_slithir_printer(solc_binary_path) -> None:
+    solc_path = solc_binary_path("0.8.0")
+    standard_json = SolcStandardJson()
+    standard_json.add_source_file(
+        Path(TEST_DATA_DIR, "test_printer_slithir", "bug-2266.sol").as_posix()
+    )
+    compilation = CryticCompile(standard_json, solc=solc_path)
+    slither = Slither(compilation)
+
+    printer = PrinterSlithIR(slither, logger=None)
+    output = printer.output("test_printer_slithir.dot")
+
+    assert "slither.core.solidity_types" not in output.data["description"]
