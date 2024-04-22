82f295f2d072f214ebf3f25f9a91fc019fbb2229
Alexis <alexis.challande@trailofbits.com>
2024-04-22 11:33:03 +0200

    Replace cache with lru_cache to keep Py3.8 compatibility

--- slither/visitors/expression/expression.py
+++ slither/visitors/expression/expression.py
@@ -1,5 +1,5 @@
 import logging
-from functools import cache
+from functools import lru_cache
 
 from slither.core.expressions.assignment_operation import AssignmentOperation
 from slither.core.expressions.binary_operation import BinaryOperation
@@ -25,7 +25,7 @@ from slither.exceptions import SlitherError
 logger = logging.getLogger("ExpressionVisitor")
 
 
-@cache
+@lru_cache()
 def get_visitor_mapping():
     """Returns a visitor mapping from expression type to visiting functions."""
     return {
