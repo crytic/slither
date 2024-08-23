aeeb2d368802844733671e35200b30b5f5bdcf5c
alpharush <0xalpharush@protonmail.com>
2024-08-23 08:20:00 -0500

    prepare 0.10.4 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Slither is a Solidity and Vyper static analysis framework written in Python 3.",
     url="https://github.com/crytic/slither",
     author="Trail of Bits",
-    version="0.10.3",
+    version="0.10.4",
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
