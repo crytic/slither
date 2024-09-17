617fe8b6faf0f372fc2f072260dbd2db9e4b940d
Goran Vladika <goran.vladika@gmail.com>
2024-09-17 12:17:26 +0200

    Add function that checks if custom error is unused

--- slither/detectors/variables/unused_custom_errors.py
+++ slither/detectors/variables/unused_custom_errors.py
@@ -4,12 +4,13 @@ Module detecting unused custom errors
 from typing import List, Dict
 
 from slither.core.compilation_unit import SlitherCompilationUnit
+from slither.core.declarations.custom_error import CustomError
+from slither.core.declarations.solidity_variables import SolidityCustomRevert
 from slither.detectors.abstract_detector import (
     AbstractDetector,
     DetectorClassification,
     DETECTOR_INFO,
 )
-from slither.formatters.variables.unused_state_variables import custom_format
 from slither.utils.output import Output
 
 class UnusedCustomErrors(AbstractDetector):
@@ -22,24 +23,31 @@ class UnusedCustomErrors(AbstractDetector):
     IMPACT = DetectorClassification.INFORMATIONAL
     CONFIDENCE = DetectorClassification.HIGH
 
-    WIKI = "https://github.com/crytic/slither/wiki/Detector-Documentation#unused-state-variable"
+    WIKI = "https://github.com/crytic/slither/wiki/Detector-Documentation#unused-custom-error"
 
-    WIKI_TITLE = "Unused state variable"
-    WIKI_DESCRIPTION = "Unused state variable."
+    WIKI_TITLE = "Unused custom error"
+    WIKI_DESCRIPTION = "Unused custom error."
     WIKI_EXPLOIT_SCENARIO = ""
-    WIKI_RECOMMENDATION = "Remove unused state variables."
+    WIKI_RECOMMENDATION = "Remove unused custom errors."
 
     def _detect(self) -> List[Output]:
-        """Detect unused state variables"""
+
+
+        """Detect unused custom errors"""
         results = []
         for c in self.compilation_unit.contracts_derived:
             for custom_error in c.custom_errors:
-                info: DETECTOR_INFO = [custom_error.name, " is never used in ", c, "\n"]
-                json = self.generate_result(info)
-                results.append(json)
-
+                if not self._isCustomErrorUsed(custom_error):
+                    info: DETECTOR_INFO = ["Unused custom error: ", custom_error.name, "\n"]
+                    json = self.generate_result(info)
+                    results.append(json)
         return results
 
-    @staticmethod
-    def _format(compilation_unit: SlitherCompilationUnit, result: Dict) -> None:
-        custom_format(compilation_unit, result)
+    def _isCustomErrorUsed(self, custom_error: CustomError) -> bool:
+        for c in self.compilation_unit.contracts_derived:
+            for f in c.functions:
+                for int_call in f._internal_calls:
+                    print("Comparing: ", int_call.name, custom_error.name)
+                    if type(int_call) is SolidityCustomRevert and custom_error.name in int_call.name:
+                        return True
+        return False
