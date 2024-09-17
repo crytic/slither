82dfd33ec0590bda7a543e6997e32f611454d2c1
Goran Vladika <goran.vladika@gmail.com>
2024-09-17 15:13:27 +0200

    Refactor and include errors defined outside of contracts

--- slither/detectors/variables/unused_custom_errors.py
+++ slither/detectors/variables/unused_custom_errors.py
@@ -1,7 +1,7 @@
 """
 Module detecting unused custom errors
 """
-from typing import List, Dict
+from typing import List, Set
 
 from slither.core.compilation_unit import SlitherCompilationUnit
 from slither.core.declarations.custom_error import CustomError
@@ -12,6 +12,7 @@ from slither.detectors.abstract_detector import (
     DETECTOR_INFO,
 )
 from slither.utils.output import Output
+
 
 class UnusedCustomErrors(AbstractDetector):
     """
@@ -31,23 +32,36 @@ class UnusedCustomErrors(AbstractDetector):
     WIKI_RECOMMENDATION = "Remove unused custom errors."
 
     def _detect(self) -> List[Output]:
-
-
         """Detect unused custom errors"""
-        results = []
-        for c in self.compilation_unit.contracts_derived:
-            for custom_error in c.custom_errors:
-                if not self._isCustomErrorUsed(custom_error):
-                    info: DETECTOR_INFO = ["Unused custom error: ", custom_error.name, "\n"]
-                    json = self.generate_result(info)
-                    results.append(json)
-        return results
+        defined_custom_errors: Set[CustomError] = set()
+        custom_reverts: Set[SolidityCustomRevert] = set()
+        unused_custom_errors: Set[CustomError] = set()
 
-    def _isCustomErrorUsed(self, custom_error: CustomError) -> bool:
-        for c in self.compilation_unit.contracts_derived:
-            for f in c.functions:
-                for int_call in f._internal_calls:
-                    print("Comparing: ", int_call.name, custom_error.name)
-                    if type(int_call) is SolidityCustomRevert and custom_error.name in int_call.name:
-                        return True
-        return False
+        # Collect all custom errors defined in the contracts
+        for contract in self.compilation_unit.contracts:
+            for custom_error in contract.custom_errors:
+                defined_custom_errors.add(custom_error)
+
+        # Add custom errors defined outside of contracts
+        for custom_error in self.compilation_unit.custom_errors:
+            defined_custom_errors.add(custom_error)
+
+        # Collect all used custom errors
+        for contract in self.compilation_unit.contracts:
+            for function in contract.functions_and_modifiers:
+                for internal_call in function.internal_calls:
+                    if isinstance(internal_call, SolidityCustomRevert):
+                        custom_reverts.add(internal_call)
+
+        # Find unused custom errors
+        for defined_error in defined_custom_errors:
+            if not any(defined_error.name in custom_revert.name for custom_revert in custom_reverts):
+                unused_custom_errors.add(defined_error)
+
+        results = []
+        for custom_error in unused_custom_errors:
+            info: DETECTOR_INFO = ["Unused custom error: # ", custom_error.full_name, "\n"]
+            json = self.generate_result(info)
+            results.append(json)
+
+        return results
