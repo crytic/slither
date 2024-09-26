2075e6f1e9f7f5ba14364db256b2e6a3517629ec
alpharush <0xalpharush@protonmail.com>
2024-09-26 18:02:35 -0500

    fmt

--- slither/slithir/operations/member.py
+++ slither/slithir/operations/member.py
@@ -33,14 +33,29 @@ class Member(OperationWithLValue):
         # Can be an ElementaryType because of bytes.concat, string.concat
         assert is_valid_rvalue(variable_left) or isinstance(
             variable_left,
-            (Contract, Enum, Function, Event, CustomError, SolidityImportPlaceHolder, ElementaryType),
+            (
+                Contract,
+                Enum,
+                Function,
+                Event,
+                CustomError,
+                SolidityImportPlaceHolder,
+                ElementaryType,
+            ),
         )
 
         assert isinstance(variable_right, Constant)
         assert isinstance(result, ReferenceVariable)
         super().__init__()
         self._variable_left: Union[
-            RVALUE, Contract, Enum, Function, Event, CustomError, SolidityImportPlaceHolder, ElementaryType
+            RVALUE,
+            Contract,
+            Enum,
+            Function,
+            Event,
+            CustomError,
+            SolidityImportPlaceHolder,
+            ElementaryType,
         ] = variable_left
         self._variable_right = variable_right
         self._lvalue = result
