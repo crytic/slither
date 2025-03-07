ad24fb824bcf91b90d6a74d08782776b507e76e3
Josselin Feist <josselin@trailofbits.com>
2025-03-07 13:06:44 +0100

    Update docs/src/tutorials/exercises/exercise2/solution.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/src/tutorials/exercises/exercise2/solution.py
+++ docs/src/tutorials/exercises/exercise2/solution.py
@@ -10,6 +10,6 @@ for contract in slither.contracts:
             continue
         if function.is_constructor:
             continue
-        if function.visibility in ['public', 'external']:
-            if not 'onlyOwner()' in [m.full_name for m in function.modifiers]:
-                print(f'{function.full_name} is unprotected!')
+        if function.visibility in ["public", "external"]:
+            if not "onlyOwner()" in [m.full_name for m in function.modifiers]:
+                print(f"{function.full_name} is unprotected!")
