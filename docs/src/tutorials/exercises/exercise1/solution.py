f7b72ef38d0c15c3734a28fac9fdd91c1c1b32bf
Josselin Feist <josselin@trailofbits.com>
2025-03-07 13:06:38 +0100

    Update docs/src/tutorials/exercises/exercise1/solution.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/src/tutorials/exercises/exercise1/solution.py
+++ docs/src/tutorials/exercises/exercise1/solution.py
@@ -5,11 +5,11 @@ coin = slither.get_contract_from_name("Coin")[0]
 
 # Iterate over all the contracts
 for contract in slither.contracts:
-   # If the contract is derived from MyContract
-   if coin in contract.inheritance:
-      # Get the function definition  
-      mint = contract.get_function_from_signature('_mint(address,uint256)')
-      # If the function was not declared by coin, there is a bug !
-      # Detect error only for contracts overriding the '_mint' function
-      if mint.contract_declarer == contract:
-           print(f'Error, {contract} overrides {mint}')
+    # If the contract is derived from MyContract
+    if coin in contract.inheritance:
+        # Get the function definition
+        mint = contract.get_function_from_signature("_mint(address,uint256)")
+        # If the function was not declared by coin, there is a bug !
+        # Detect error only for contracts overriding the '_mint' function
+        if mint.contract_declarer == contract:
+            print(f"Error, {contract} overrides {mint}")
