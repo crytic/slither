133db130c10f5f5bc50d7743117a7584227b6af9
Josselin Feist <josselin@trailofbits.com>
2025-03-07 13:05:59 +0100

    Update docs/src/api/examples/print_basic_information.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/src/api/examples/print_basic_information.py
+++ docs/src/api/examples/print_basic_information.py
@@ -6,7 +6,7 @@ slither = Slither("coin.sol")
 
 for contract in slither.contracts:
     # Print the contract's name
-    print(f'Contract: {contract.name}')
+    print(f"Contract: {contract.name}")
     # Print the name of the contract inherited
     print(f'\tInherit from{[c.name for c in contract.inheritance]}')
     for function in contract.functions:
