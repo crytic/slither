079ec2b2d0f885b26ca7d6216dc6fa361b1941d1
tsinghuacoder <tsinghuacoder@icloud.com>
2025-04-11 17:11:48 +0800

    chore: fix some typos in comment
    Signed-off-by: tsinghuacoder <tsinghuacoder@icloud.com>

--- scripts/ci_test_data_dependency.sh
+++ scripts/ci_test_data_dependency.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-### Test data dependecy
+### Test data dependency
 
 if ! python ./examples/scripts/data_dependency.py ./examples/scripts/data_dependency.sol; then
     echo "data dependency failed"
