8158d2d2a3a8890389d7d5c35c1b504b40bacc9f
Marcelo Morales <morales.374@osu.edu>
2025-09-09 16:04:12 -0400

    interval-df: adding placeholder analysis and domain

---
+++ slither/analyses/data_flow/analyses/interval/analysis/analysis.py
@@ -0,0 +1,6 @@
+from slither.analyses.data_flow.engine.analysis import Analysis
+
+
+class IntervalAnalysis(Analysis):
+    def __init__(self):
+        super().__init__()
---
+++ slither/analyses/data_flow/analyses/interval/analysis/domain.py
@@ -0,0 +1,6 @@
+from slither.analyses.data_flow.engine.domain import Domain
+
+
+class IntervalDomain(Domain):
+    def __init__(self):
+        pass
