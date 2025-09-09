8a05e922dca2c3fda07a468b3bce4d596925576f
Marcelo Morales <morales.374@osu.edu>
2025-09-09 16:17:30 -0400

    interval-df: adding placeholder for detector

--- slither/detectors/all_detectors.py
+++ slither/detectors/all_detectors.py
@@ -20,6 +20,7 @@ from .reentrancy.reentrancy_no_gas import ReentrancyNoGas
 from .reentrancy.reentrancy_events import ReentrancyEvent
 from .reentrancy_df.reentrancy_eth_df import ReentrancyEthDF
 
+
 from .variables.unused_state_variables import UnusedStateVars
 from .variables.could_be_constant import CouldBeConstant
 from .variables.could_be_immutable import CouldBeImmutable
---
+++ slither/detectors/interval_analysis/interval_analysis_df.py
@@ -0,0 +1,47 @@
+"""
+Interval analysis detection
+"""
+
+from ast import Dict, Set
+from collections import defaultdict, namedtuple
+from dataclasses import dataclass
+from typing import Any, List, Optional
+from slither.analyses.data_flow.analyses.interval.analysis.analysis import IntervalAnalysis
+from slither.analyses.data_flow.analyses.interval.analysis.domain import IntervalDomain
+from slither.analyses.data_flow.engine.engine import Engine
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+
+from slither.utils.output import Output
+
+
+@dataclass
+class FindingKey:
+    attr1: Optional[Any] = None
+
+
+@dataclass
+class FindingValue:
+    attr1: Optional[Any] = None
+
+
+class IntervalAnalysisDF(AbstractDetector):
+    ARGUMENT = "interval-analysis-df"
+    HELP = "Interval analysis detection"
+    IMPACT = DetectorClassification.HIGH
+    CONFIDENCE = DetectorClassification.MEDIUM
+    WIKI = "tbd"
+    WIKI_TITLE = "tbd"
+    WIKI_DESCRIPTION = "tbd"
+    WIKI_EXPLOIT_SCENARIO = "tbd"
+    WIKI_RECOMMENDATION = "tbd"
+    STANDARD_JSON = False
+
+    def find_intervals(self) -> Dict[FindingKey, Set[FindingValue]]:
+        result: Dict[FindingKey, Set[FindingValue]] = {}
+
+        return result
+
+    def _detect(self) -> List[Output]:
+        super()._detect()
+        results: List[Output] = []
+        return results
