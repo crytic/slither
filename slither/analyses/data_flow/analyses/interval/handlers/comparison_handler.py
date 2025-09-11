573a24c9f3b75b6701cf015b4ecace0c866f8938
Marcelo Morales <morales.374@osu.edu>
2025-09-11 09:19:53 -0400

    feat(interval): adding comparison handler boiler

--- slither/analyses/data_flow/analyses/interval/handlers/comparison_handler.py
+++ slither/analyses/data_flow/analyses/interval/handlers/comparison_handler.py
@@ -1,8 +1,32 @@
+from typing import Union
 from slither.analyses.data_flow.analyses.interval.analysis.domain import IntervalDomain
+from slither.analyses.data_flow.analyses.interval.managers.variable_info_manager import (
+    VariableInfoManager,
+)
 from slither.core.cfg.node import Node
-from slither.slithir.operations.binary import Binary
+from slither.slithir.operations.binary import Binary, BinaryType
+
+from loguru import logger
 
 
 class ComparisonHandler:
+
+    def __init__(self):
+        pass
+
     def handle_comparison(self, node: Node, domain: IntervalDomain, operation: Binary):
+        if not isinstance(
+            operation.type,
+            Union[
+                BinaryType.GREATER,
+                BinaryType.LESS,
+                BinaryType.GREATER_EQUAL,
+                BinaryType.LESS_EQUAL,
+                BinaryType.EQUAL,
+                BinaryType.NOT_EQUAL,
+            ],
+        ):
+            logger.error("Comparison operation type is not a valid comparison type")
+            raise ValueError("Comparison operation type is not a valid comparison type")
+
         pass
