0839fafc6ffd3989d7e404a828a384e334c92e13
Emilio López <emilio.lopez@trailofbits.com>
2025-11-25 17:30:40 -0300

    tests: fix slither_config

--- scripts/ci_test_slither_config.sh
+++ scripts/ci_test_slither_config.sh
@@ -2,7 +2,9 @@
 
 ### Test
 
-if ! slither "tests/*.json" --config "tests/config/slither.config.json"; then
+solc-select use 0.7.0
+
+if ! slither "tests/e2e/config/test_json_config/test.sol" --config "tests/e2e/config/test_json_config/slither.config.json"; then
     echo "Config failed"
     exit 1
 fi
